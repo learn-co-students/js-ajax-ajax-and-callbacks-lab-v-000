0969013657a56e2b9630331d8f9ad1064e5ac348
Logan McAnsh <logan@mcan.sh>
2017-10-02 16:23:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {});
+$(document).ready(() => {});
 
 function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.");
@@ -6,46 +6,39 @@ function displayError() {
 
 function searchRepositories() {
   const searchTerms = $('#searchTerms')[0].value;
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, (data) => {
     $('#results').html(showRepositories(data));
   }).fail(displayError());
 }
 
 function showRepositories(repos) {
-  var result =
-    '<ul>' +
-    repos.items
-      .map(r => {
-        return `
+  const result = `<ul>${repos.items
+    .map(r => `
           <li>
             <h2><a href="${r.html_url}">${r.name}</a></h2>
             <p>Description: ${r.description}</p>
          	<p><a href="${r.owner.url}">${r.owner.login}</a></p>
          	<img src="${r.owner.avatar_url}" height="32" width="32">
         	<p><a href="#" data-repository="${r.name}" data-owner="${r.owner
-          .login}" onclick="showCommits(this)">Show Commits</a></p>
-          </li>`;
-      })
-      .join('') +
-    '</ul>';
+  .login}" onclick="showCommits(this)">Show Commits</a></p>
+          </li>`,)
+    .join('')}</ul>`;
   return result;
 }
 
 function showCommits(element) {
-  var owner = element.dataset.owner;
-  var repo = element.dataset.repository;
-  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+  const owner = element.dataset.owner;
+  const repo = element.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, (data) => {
     $('#details').html(displayCommits(data));
   }).fail(displayError());
 }
 
 function displayCommits(data) {
-  var result = data
-    .map(r => {
-      return `<ul><li>
+  const result = data
+    .map(r => `<ul><li>
   		<h3>${r.sha}</h3><p>${r.commit.message}</p>
-  		</li></ul>`;
-    })
+  		</li></ul>`,)
     .join('');
   return result;
 }
