4541635d5cf735026a94a361743b85b23ce3fed6
Timothy Cevallos <timcev@gmail.com>
2017-10-12 19:35:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 $(document).ready(function (){
 });
+
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $(`#results`).html(showRepositories(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function showRepositories(result){
+  let repos = '<ul>' + result.items.map(r => {
+    return (`<li>
+      <a href="${r.html_url}">${r.name}</a>
+      <p>${r.description}${r.owner.url}${r.owner.login}</p>
+      <img src="${r.owner.avatar_url}">
+      <a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a>
+      </li>`)
+  }).join('') + "</ul>"
+  return repos
+}
+
+function showCommits(el){
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data =>{
+    $("#details").html(displaycommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function displaycommits(data){
+  const result = data.map((r) =>{
+    return `
+    <h3>${r.sha}</h3>
+    <p>${r.commit.message}</p>
+    `
+  })
+  return result;
+}
