9a6cd2b91ccf586e3b76ad373d6684ed438675d7
laramontana <larabrightside@gmail.com>
2017-11-14 19:34:33 +0300

    Done.

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ $(document).ready(function (){
 function searchRepositories() {
   const searchTerms = $("#searchTerms")[0].value;
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, response => {
-    // console.log(response.items);
-    const results = response.items
-    const src = $("#results-template")
-    const template = Handlebars.compile(src)
-    const resultsInTemplate = template(results)
-    $("#results").html(resultsInTemplate);
-  }).done();
+    debugger
+    $('#results').html(renderSearchResults(response))
+  })
+}
+
+function renderSearchResults(response) {
+  return response.items.map(item => {
+    return `
+      <div>
+        <h2><a href="${item.html_url}">${item.name}</a></h2>
+        <p><a href="#" data-repository="${item.name}" data-owner="${item.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${item.description}</p>
+      </div>
+      <hr>
+    `
+  })
 }
