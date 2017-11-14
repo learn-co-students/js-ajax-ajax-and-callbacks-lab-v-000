8b0c01f0f2b0b86d2abb315728de0cbf1a75b306
laramontana <larabrightside@gmail.com>
2017-11-14 13:43:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 $(document).ready(function (){
+
 });
+
+function searchRepositories() {
+  const searchTerms = $("#searchTerms")[0].value;
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, response => {
+    // console.log(response.items);
+    const results = response.items
+    const src = $("#results-template")
+    const template = Handlebars.compile(src)
+    const resultsInTemplate = template(results)
+    $("#results").html(resultsInTemplate);
+  }).done();
+}
