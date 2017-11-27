0ac06d88bdf317add841819f03af3c16ebade80e
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-27 10:15:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 $(document).ready(function (){
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML);
 });
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val();
+  const rootUri = "https://api.github.com/search/repositories?q="
+  
+  $.get(rootUri + searchTerms, function(data) {
+  })
+  .done(function(data) {
+    showRepositories(data);
+  })
+  .fail(function(error){
+    displayError();
+  });
+}
+
+function showRepositories(data) {
+  const src = document.getElementById('repository-template').innerHTML;
+  const template = Handlebars.compile(src);
+  const repoList = template(data.items);
+  document.getElementById('results').innerHTML = repoList;
+}
+
+function showCommits(el) {
+  const repository = `${el.dataset.owner}/${el.dataset.repository}`;
+  $.get(`https://api.github.com/repos/${repository}/commits`, function(data) {
+  })
+  .done(function(data) {
+    const src = document.getElementById('commit-template').innerHTML;
+    const template = Handlebars.compile(src);
+    const commitList = template(data);
+    document.getElementById('details').innerHTML = commitList;
+  })
+  .fail(function(error) {
+    displayError();
+  });
+}
+
+function displayError() {
+  $('#errors').append("<strong>I'm sorry, there's been an error. Please try again.</strong>");
+}
