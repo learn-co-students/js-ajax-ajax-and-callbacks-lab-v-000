e7283036e0b7bce685846e5484df685fdf4e56c7
¨anniezh12¨ <aniqaelahi@yahoo.com>
2017-12-14 20:41:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-$(document).ready(function (){
+$(document).ready(function(){
+
+var searchRepositories = ()=> {
+  let userinput = document.getElementById("searchTerms").value;
+  console.log("hello");
+  $.get(`https://api.github.com/search/repositories?q=${userinput}`, function(data)  {
+    let result = data.items.map(item => { return `<p>Name:<a href="${item.html_url}">${item.full_name})</a>`});
+     $("#results").html(result);
+  }).fail(displayError);
+}
+function showCommits() {
+  console.log("test")
+}
+
+function displayError(){
+  console.log("I'm sorry, there's been an error. Please try again." );
+}
 });
