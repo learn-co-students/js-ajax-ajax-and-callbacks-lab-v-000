0e840c56fd1f228557cb77c8e2556c99f29fc5e9
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-25 15:37:56 -0800

    jquery'd up the searching fcn

--- index.js
+++ index.js
@@ -5,62 +5,92 @@ document.addEventListener("DOMContentLoaded", function(event) {
   Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
 });
 
+// function searchRepositories() {
+//   const searchTerms = $('#searchTerms').val();
+//   const req = new XMLHttpRequest();
+//   req.addEventListener("load", displaySearchResults);
+//   req.open("GET", `https://api.github.com/search/repositories?q=${searchTerms}`);
+//   req.send();
+// }
+
 function searchRepositories() {
   const searchTerms = $('#searchTerms').val();
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", displaySearchResults);
-  req.open("GET", `https://api.github.com/search/repositories?q=${searchTerms}`);
-  req.send();
+  const apiRequest = `https://api.github.com/search/repositories?q=${searchTerms}`;
+
+  $.get(apiRequest, function(resp) {
+      const searchResults = [];
+    for (const repo of resp.items) {
+      let repoHash = {};
+      repoHash.name = repo.name;
+      repoHash.description = repo.description;
+      repoHash.html_url = repo.html_url;
+      repoHash.ownerSite = repo.owner.html_url;
+      repoHash.ownerLogin = repo.owner.login;
+      repoHash.ownerPic = repo.owner.avatar_url;
+      searchResults.push(repoHash);
+    }
+    displaySearchResults(searchResults);
+  });
 }
 
-function displaySearchResults() {
-  const searchResults = JSON.parse(this.responseText).items;
+function displaySearchResults(searchResults) {
   const searchList = "<ul>" + searchResults.map(repo => {
     return(`
           <li>
             <h2>${repo.name}</h2>
             <b>Description: </b>${repo.description} <br>
             <b>Repo Link: </b><a href= ${repo.html_url}>${repo.html_url}</a> <br>
-            <b>Owner: </b><a href=${repo.owner.html_url}>${repo.owner.login}</a><br>
-            <img src="${repo.owner.avatar_url}" height="32" width="32"> <br>
+            <b>Owner: </b><a href=${repo.ownerSite}>${repo.ownerLogin}</a><br>
+            <img src="${repo.ownerPic}" height="32" width="32"> <br>
           </li>`)
   }).join('') + "</ul>";
   document.getElementById("results").innerHTML = searchList;
 }
 
-// resp.items.length // 30
+// function displaySearchResults() {
+//   const searchResults = JSON.parse(this.responseText).items;
+//   const searchList = "<ul>" + searchResults.map(repo => {
+//     return(`
+//           <li>
+//             <h2>${repo.name}</h2>
+//             <b>Description: </b>${repo.description} <br>
+//             <b>Repo Link: </b><a href= ${repo.html_url}>${repo.html_url}</a> <br>
+//             <b>Owner: </b><a href=${repo.owner.html_url}>${repo.owner.login}</a><br>
+//             <img src="${repo.owner.avatar_url}" height="32" width="32"> <br>
+//           </li>`)
+//   }).join('') + "</ul>";
+//   document.getElementById("results").innerHTML = searchList;
+// }
+
+function showCommits(el) {
+
+  //document.getElementById("details").innerHTML = something;
+}
+
+// function getRepositories() {
+//   const username = document.getElementById("username").value;
+//   const req = new XMLHttpRequest();
+//   req.addEventListener("load", displayRepositories);
+//   req.open("GET", `https://api.github.com/users/${username}/repos`);
+//   req.send();
+// }
 //
-// resp.items[0].name; //react-tetris
-// resp.items[0].description;// use react, redux, immutable to code...
-// resp.items[0].html_url; // 'https://github.com/chvin/react-tetris'
-// resp.items[0].owner.login; //'chvin';
-// resp.items[0].owner.avatar_url; // 'https://avatars2.githubusercontent.com/u/5383506?v=4'
-// resp.items[0].owner.html_url; // 'https://github.com/chvin'
-
-function getRepositories() {
-  const username = document.getElementById("username").value;
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", displayRepositories);
-  req.open("GET", `https://api.github.com/users/${username}/repos`);
-  req.send();
-}
-
-function displayRepositories() {
-  const repos = JSON.parse(this.responseText)
-  const repoList = "<ul>" + repos.map(repo => {
-    const dataUsername = 'data-username="' + repo.owner.login + '"'
-    const dataRepoName = 'data-repository="' + repo.name + '"'
-    return(`
-          <li>
-            <h2>${repo.name}</h2>
-            <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-          </li>`
-          )
-  }).join('') + "</ul>";
-  document.getElementById("repositories").innerHTML = repoList
-}
+// function displayRepositories() {
+//   const repos = JSON.parse(this.responseText)
+//   const repoList = "<ul>" + repos.map(repo => {
+//     const dataUsername = 'data-username="' + repo.owner.login + '"'
+//     const dataRepoName = 'data-repository="' + repo.name + '"'
+//     return(`
+//           <li>
+//             <h2>${repo.name}</h2>
+//             <a href="${repo.html_url}">${repo.html_url}</a><br>
+//             <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+//             <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+//           </li>`
+//           )
+//   }).join('') + "</ul>";
+//   document.getElementById("repositories").innerHTML = repoList
+// }
 
 function loadSearchTemplate() {
   const searchTerm = document.getElementById("search-form-template").innerHTML;
