00549b741172887892849770c076a21a53cfe42f
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-25 11:22:13 -0800

    basic vanilla script loading learnt

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  debugger;
+  const searchTerms = $('#searchTerms').val;
+}
+
+function getRepositories() {
+  const username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", displayRepositories);
+  req.open("GET", `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`
+          )
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function loadSearchTemplate() {
+  const searchTerm = document.getElementById("search-form-template").innerHTML;
+  document.getElementById("searchBox").innerHTML = searchTerm;
+}
