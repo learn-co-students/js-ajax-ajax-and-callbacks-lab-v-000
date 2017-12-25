f25ce51fb54ff23829d6240e90bef9341be419b7
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-25 12:21:39 -0800

    searching working

--- index.js
+++ index.js
@@ -2,9 +2,37 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-  debugger;
-  const searchTerms = $('#searchTerms').val;
+  const searchTerms = $('#searchTerms').val();
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", displaySearchResults);
+  req.open("GET", `https://api.github.com/search/repositories?q=${searchTerms}`);
+  req.send();
 }
+
+function displaySearchResults() {
+  const searchResults = JSON.parse(this.responseText).items;
+  const searchList = "<ul>" + searchResults.map(repo => {
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            ${repo.description} <br>
+            ${repo.html_url} <br>
+            ${repo.owner.login} <br>
+            ${repo.owner.avatar_url} <br>
+            ${repo.owner.html_url}
+          </li>`)
+  }).join('') + "</ul>";
+  document.getElementById("results").innerHTML = searchList;
+}
+
+// resp.items.length // 30
+//
+// resp.items[0].name; //react-tetris
+// resp.items[0].description;// use react, redux, immutable to code...
+// resp.items[0].html_url; // 'https://github.com/chvin/react-tetris'
+// resp.items[0].owner.login; //'chvin';
+// resp.items[0].owner.avatar_url; // 'https://avatars2.githubusercontent.com/u/5383506?v=4'
+// resp.items[0].owner.html_url; // 'https://github.com/chvin'
 
 function getRepositories() {
   const username = document.getElementById("username").value;
@@ -35,12 +63,3 @@ function loadSearchTemplate() {
   const searchTerm = document.getElementById("search-form-template").innerHTML;
   document.getElementById("searchBox").innerHTML = searchTerm;
 }
-
-// resp.items.length // 30
-//
-// resp.items[0].name; //react-tetris
-// resp.items[0].description;// use react, redux, immutable to code...
-// resp.items[0].html_url; // 'https://github.com/chvin/react-tetris'
-// resp.items[0].owner.login; //'chvin';
-// resp.items[0].owner.avatar_url; // 'https://avatars2.githubusercontent.com/u/5383506?v=4'
-// resp.items[0].owner.html_url; // 'https://github.com/chvin'
