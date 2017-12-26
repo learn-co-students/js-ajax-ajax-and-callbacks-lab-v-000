e9377b30ec79b320337c079239954487ddfa95f9
Soumya Veer <veer.soumya@gmail.com>
2017-12-26 21:52:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+function displayError() {
+  $('#errors').html("Error!! Please try again.");
+}
+
+function renderSearchResult (result){
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+};
+
+let renderSearchResults = (data) => data.items.map( result => renderSearchResult(result));
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val();
+  let apiUrl = `https://api.github.com/search/repositories?q=${searchTerms}`;
+  $.get(apiUrl, data => {
+    $('#results').html(renderSearchResults(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function renderCommit (commit){
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+function renderCommits(data) {
+  let result = data.map((commit) => renderCommit(commit)).join("");
+  return `<ul>${result}</ul>`
+}
+
+function showCommits(el) {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data));
+  }).fail(error => {
+    displayError()
+  })
+}
+
 $(document).ready(function (){
 });
