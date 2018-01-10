8f217a2d61c1fc353e7df9dcb7e5642781cd4c45
Adam Tropp <adt6261@gmail.com>
2018-01-10 01:26:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+var url = 'api.github.com/search/repositories'
+var searchTerms = $('#searchTerms')
+
 $(document).ready(function (){
+  $.get(url + "/" + searchTerms, function(response) {
+    searchRepositories();
+  }).fail(function(error) {
+    displayError();
 });
+
+function searchRepositories() {
+ $("results")
+   .append("Name" + response.name)
+   .append("Description" + response.description)
+   .append("URL" + response.url)
+   .append(response.owner.login)
+   .append(response.owner.avatar)
+   .append(response.owner.homepage)
+ })
+
+ function displayError() {
+   $('#errors').innerHTML += "I'm sorry, there's been an error. Please try again."
+ }
