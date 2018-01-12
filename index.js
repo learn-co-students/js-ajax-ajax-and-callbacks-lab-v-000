69c28979a1e0395ed32c23367fbbbe685fcc22c9
Heather You <heatherhyou@gmail.com>
2018-01-12 11:25:05 -0500

    Done.

--- index.js
+++ index.js
@@ -2,32 +2,48 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-  const term = document.getElementById("searchTerms").value;
-  const uri = "https://api.github.com/search/repositories?q=" + term;
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", displayRepositories);
-  xhr.open("GET", uri)
-  xhr.send();
-  return false;
+  const searchTerms = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(renderSearchResults(data))
+  }).fail(displayError);
 }
 
-function displayRepositories() {
-  const repos = JSON.parse(this.responseText);
-  const repoList = "<ul>" + repos.map(repo => {
-    const dataUsername = 'data-username="' + repo.owner.login + '"'
-    const dataRepoName = 'data-repository="' + repo.name + '"'
-    return(`
+function renderSearchResults(data) {
+  const repoList = '<ul>' + data.items.map(repo => {
+    return (`
       <li>
-        <h2>${repo.name}</h2>
-        <a href="${repo.html_url}">${repo.html_url}</a><br>
-        <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-        <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-        </li>`
-      )
-  }).join('') + "</ul>";
-  document.getElementById("repositories").innerHTML = repoList
+        <h2><a href="$data.html_url">${repo.name}</a></h2>
+        <p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this); return false;">Show Commits</a></p>
+        <p>${repo.description}</p>
+      <li>
+    `)
+  }).join('') + '</ul>';
+  return repoList
 }
 
+function showCommits(el) {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(data) {
+    $('#details').html(renderCommits(data))
+  }).fail(displayError);
+}
+
+function renderCommits(data) {
+  const results = '<ul>' + data.map(commit => {
+    return (`
+      <li>
+        <p>SHA: ${commit.sha}</p>
+        <h3>Commits By: ${commit.commit.author.name}</h3>
+        <h4>${commit.author.login}</h4>
+        <img src="${commit.author.avatar_url}" height="32" width="32">
+      </li>
+    `)
+  }).join('') + '</ul>';
+  return results
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
 
 /*Display the collection of repositories inside the results div.
 Include repository name, description, and a link to the HTML URL.
