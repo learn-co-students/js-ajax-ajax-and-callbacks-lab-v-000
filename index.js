7fcd6720fa26b2a7b9ebf7e4b7d37f951630e1f4
hyoyou <heatherhyou@gmail.com>
2018-01-12 09:39:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const term = document.getElementById("searchTerms").value;
+  const uri = "https://api.github.com/search/repositories?q=" + term;
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", displayRepositories);
+  xhr.open("GET", uri)
+  xhr.send();
+  return false;
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+      <li>
+        <h2>${repo.name}</h2>
+        <a href="${repo.html_url}">${repo.html_url}</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+        </li>`
+      )
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
