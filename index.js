fc5d3e3a81cb072c64ddd1f19c6b6050e762ba77
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 20:56:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get("https://api.github.com/search/repositories?q=" + searchTerms, displayResults).fail(displayError)
+}
+
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function displayResults(data) {
+  const resultsHTML = data.items.map (result =>
+    ` <a href="${result.html_url}">${result.name}</a>
+      <p><a href="#" data-url="${result.url}/commits" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      <p>${result.description}</p>`)
+  $("#results").html(resultsHTML)
+}
+
+function showCommits(el) {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function (data) {
+    const commitsHTML = "<ul>" + data.map(commit => `<li> ${commit.commit.author.name} - ${commit.commit.message} - ${commit.sha}</li>`).join('') + "</ul>";
+    $('#details').html(commitsHTML);
+  })
+}
+
 $(document).ready(function (){
+
 });
