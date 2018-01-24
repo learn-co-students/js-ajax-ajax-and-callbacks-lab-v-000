347d957238c86da3bf521d7b614b91e07543d3d3
Jelo Yambao <jelolearns@gmail.com>
2018-01-24 22:31:28 +0000

    Done.

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ function displayError() {
 
 function showCommits(responseItems) {
   $.get(`http://api.github.com/repos/${responseItems.owner.login}/${responseItems.name}/commits`, function(response) {
-    console.log(response);
-    debugger
-    $("#details").html(" ")
     $("#details").html(`<ul>${response.map(r => '<li>Sha: ' + r.sha + '<p>Author: ' + r.commit.author.name + `</p>  <p>Username: ` + `${r.author ? r.author.login : 'Unavailable'}` + '</p>' + `<img style=" width: 150px; height: 150px;" src="${r.author.avatar_url}" alt="avatar"> </li><br>`).join('')}</ul>`);
+  }).fail(function(error) {
+    displayError();
   })
+}
+
+function showRepository(data) {
+  that = data
+  $("#results").append(`<h2>Repo Name: <a href="${data.html_url}">${data.name}</a></h2><br><img style=" width: 150px; height: 150px;" src="${data.owner.avatar_url}" alt="avatar"> <p>By: <a href="${data.owner.url}">${data.owner.login}</a></p> <a href="#" onclick="showCommits(that)">Show Commits</a>`);
 }
 
 function searchRepositories() {
   $.get(`https://api.github.com/search/repositories?q=${$("input[name~='searchTerms']").val()}`, function(response) {
     $("#results").html(" ")
     for (i = 0; i < response.items.length; i++) {
-      that = response.items[i]
-      console.log(response.items[i]);
-      $("#results").append(`<h2>Repo Name: <a href="${response.items[i].html_url}">${response.items[i].name}</a></h2><br><img style=" width: 150px; height: 150px;" src="${response.items[i].owner.avatar_url}" alt="avatar"> <p>By: <a href="${response.items[0].owner.url}">${response.items[0].owner.login}</a></p> <a href="#" onclick="showCommits(that)">Show Commits</a>`);
+      showRepository(response.items[i])
     }
   }).fail(function(error) {
     displayError();
