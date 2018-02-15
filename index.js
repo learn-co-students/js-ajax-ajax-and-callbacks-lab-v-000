50311670e2a94477e64d1c947a411c7011c592c2
github username <github email address>
2018-02-15 16:56:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-$(document).ready(function (){
+// We should wait for the page to load before running our Ajax request
+$(document).ready(function(){
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  $.get("https://developer.github.com/v3/", function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    $("#searchTerms").html(response);
+  });
 });
