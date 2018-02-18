c7df7b6b1a62ddff377c432768e880cc87d7e9a7
github username <schun08@gmail.com>
2018-02-17 19:33:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 $(document).ready(function (){
+  searchRepositories().done()
 });
+
+function searchRepositories() {
+  const url = 'https://api.github.com/search/repositories?q=' + document.getElementById("searchTerms").value;
+  $.get(url, function(data){
+    displayResults();
+  });
+}
+
+function displayResults(){
+  // repo name, description, link to HTML URL
+  const results = JSON.parse(this.responseText);
+  const resultList = `<ul>${results.map(r => '<li>' + '<a href="' + r.html_url + '>' + r.name + '</a> - Description: ' + r.description).join("")}</ul>`;
+  document.getElementById("results").innerHTML = resultList;
+}
