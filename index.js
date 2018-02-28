27349faa2936e4e492273c40944d6bd79d8e31e2
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-02-28 02:27:01 +0000

    Done.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function renderSingleResult(r) {
             <h2>Name: ${r.name}</a></h2>
             <a href="${r.html_url}">HTML URL</a>
             <p>Description: ${r.description}</p>
-            <p>Login: ${r.owner.login}</p>
+            <p id="login">Login: ${r.owner.login}</p>
             <img src="${r.owner.avatar_url}">
             <a href="${r.owner.html_url}">Profile page</a><br>
-            <a href="#" onclick="showCommits(${r});">Show Commits</a>
+            <a href="#" data-repository="${r.name}"  data-owner="${r.owner.login}" onclick="showCommits(this);">Show Commits</a>
         </li>
         `;
     
@@ -32,15 +32,19 @@ function displayError() {
 
 // https://api.github.com/repos/PSNB92/Tetris/commits
 function showCommits(r) {
-    debugger
-    var url = "https://api.github.com/repos/" +  r.owner.login + "/" +r.name + "/commits"
+    
+    // console.log(dataset.owner)
+    var url = "https://api.github.com/repos/" +  r.dataset.owner + "/" + r.dataset.repository + "/commits";
+    // debugger
     $.get(url, data => {
         // debugger
        $("#details").html(renderCommits(data));
     }).fail(function(error) {displayError()});
 }
 
-function renderCommits(r){
+
+var renderCommits = (data) => data.map(r => renderSingleCommit(r))
+function renderSingleCommit(r){
     return `
             <li>  
                 <p>SHA: ${r.sha}</p>
