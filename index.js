780412adb7540377cc9ed51044a7c08d89b28094
Hannah Carnes <hecarnes@gmail.com>
2018-03-04 21:29:09 -0500

    search shows repositories

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-$(document).ready(function (){
+function searchRepositories(event) {
+  const q = $("#searchTerms").val()
+
+  const jqXhr = $.get("https://api.github.com/search/repositories", {
+    q
+  })
+  jqXhr.done(function (data) {
+    const resultsHtml = data.items.map(repo => `${repo.name}: ${repo.description}`).join("<br>")
+    $("#results").html(resultsHtml)
+  })
+}
+$(document).ready(() => {
+  $("#search").click(searchRepositories)
 });
