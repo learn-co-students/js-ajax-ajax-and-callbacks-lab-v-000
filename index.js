d62851493e0ddc11e93269e25ed45f64a43d0c9e
Christian Egglin <cegglin@conncoll.edu>
2018-03-08 21:32:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+function showCommits (linkContainingRepoInfo) {
+  const repoName = linkContainingRepoInfo.dataset.repository;
+  const owner = linkContainingRepoInfo.dataset.owner;
+  const url = `https://api.github.com/repos/${owner}/${repoName}/commits`
+  $.get(url, function (data) {
+    const templateSrcCode = document.getElementById("commits-template").innerHTML;
+    const templateFn = Handlebars.compile(templateSrcCode);
+    const commitsList = templateFn(data);
+    $("#details").html(commitsList);
+  }).fail(displayError)
+}
+
+function displayError (error) {
+  $("#errors").html(`<p>I'm sorry, there's been an error. Please try again.</p>`);
+}
+
+function searchRepositories () {
+  const searchTerm = document.getElementById("searchTerms").value;
+  const url = `https://api.github.com/search/repositories?q=${searchTerm}`;
+  $.get(url, function (data) {
+    const templateSrcCode = document.getElementById("repository-template").innerHTML;
+    const templateFn = Handlebars.compile(templateSrcCode);
+    const repoList = templateFn(data.items);
+    $("#results").html(repoList);
+  }).fail(displayError)
+}
+
 $(document).ready(function (){
 });
