13513da174ccfa3b79d6ef1bd1a84cac533b3990
chrisjamesr <chrisjames.r@gmail.com>
2018-03-18 23:09:36 -0400

    add searchRepositories and showRepositories functions. Both working correctly

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 $(document).ready(function (){
 });
+
+// const gitHubUrl = 'https://api.github.com/search/repositories' 
+
+function searchRepositories(){
+  searchTerm = $("#searchTerms")[0].value
+  $.get("https://api.github.com/search/repositories" , {q : searchTerm}, showRepositories)
+} 
+
+function showRepositories(response){
+  repos = '<ul>' + response.items.map(r=> {
+    return (`
+    <li>
+    <a href="${r.html_url}">${r.name}</a> - ${r.description || "No Description"}
+    </p>`
+    )
+  }).join("") + '</ul>' 
+  $("#results")[0].innerHTML = repos
+}
+
+function searchCommits(){
+
+}
+
+function showCommits(){
+
+}
