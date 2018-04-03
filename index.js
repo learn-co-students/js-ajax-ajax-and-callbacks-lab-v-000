3943913373b6ba2b9e5abb5ffe0e7c88c700c4af
mevanstron <michael@michaelgregoryevans.com>
2018-04-03 16:00:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 function searchRepositories() {
   const searchTerms = document.getElementById("searchTerms").value;
-  console.log(searchTerms);
-  console.log(`https://api.github.com/search/repositories?q=${searchTerms}`)
-
-  $(document).ready(function() {
-    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response) {
-
-      //console.log(response.items[0]);
-      const repos = response.items
-      const repoList = '<ul>' + repos.map(
-          r => {
-            return (`
-              <li>
-                <p>${r.name}<br>
-                - ${r.description}<br>
-                - <a href="${r.html_url}" target="_blank">${r.html_url}</a></p>
-              </li>
-            `)
-          }
-        ).join('') + '</ul>';
-      $("#results").html(repoList);
-    })
-    .fail(function(error) {
-      console.log('Something went wrong: ' + error);
-      displayError();
-    });
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response) {
+    const repos = response.items;
+    const repoList = '<ul>' + repos.map(
+        r => {
+          return (`
+            <li>
+              <p>${r.name}<br>
+              - ${r.description}<br>
+              - ${r.owner.login}<br>
+              - <img src="${r.owner.avatar_url}" width ="32" height="32"><br>
+              - <a href="${r.html_url}" target="_blank">${r.html_url}</a><br>
+              - <a href="#" data-owner="${r.owner.login}" data-repository="${r.name}" onclick="showCommits(this)">Get Commits</a></p>
+            </li>
+          `);
+        }
+      ).join('') + '</ul>';
+    $("#results").html(repoList);
+  }).fail(function(error) {
+    displayError();
   });
-
-
 }
 
 function displayError() {
   document.getElementById("errors").innerHTML += "I'm sorry, there's been an error.";
 }
+
+function showCommits(el) {
+  const repo = el.dataset.repository;
+  const owner = el.dataset.owner;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(response) {
+    const commitList = '<ul>' + response.map(
+      r => {
+      //  console.log(r)
+        return (`
+          <li>
+
+            <p>${r.commit.author.name}<br>
+            - ${r.author ? r.author.login : ""}<br>
+            - ${r.author ? '<img src="' + r.author.avatar_url + '" width="32" height="32">' : ""}<br>
+            - ${r.sha}</p>
+          </li>
+        `);
+      }
+    ).join('') + '</ul>';
+    $("#details").html(commitList);
+  });
+}
+
+$(document).ready(function() {
+});
