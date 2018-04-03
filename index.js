a1c78b9a36056e95e5f378cbbbe01f158d923571
mevanstron <michael@michaelgregoryevans.com>
2018-04-03 13:50:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-$(document).ready(function (){
-});
+function searchRepositories() {
+  const searchTerms = document.getElementById("searchTerms").value;
+  console.log(searchTerms);
+  console.log(`https://api.github.com/search/repositories?q=${searchTerms}`)
+
+  $(document).ready(function() {
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response) {
+
+      //console.log(response.items[0]);
+      const repos = response.items
+      const repoList = '<ul>' + repos.map(
+          r => {
+            return (`
+              <li>
+                <p>${r.name}<br>
+                - ${r.description}<br>
+                - <a href="${r.html_url}" target="_blank">${r.html_url}</a></p>
+              </li>
+            `)
+          }
+        ).join('') + '</ul>';
+      $("#results").html(repoList);
+    })
+    .fail(function(error) {
+      console.log('Something went wrong: ' + error);
+      displayError();
+    });
+  });
+
+
+}
+
+function displayError() {
+  document.getElementById("errors").innerHTML += "I'm sorry, there's been an error.";
+}
