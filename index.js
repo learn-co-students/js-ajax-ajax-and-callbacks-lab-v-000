81bad69f6c23e2e2730dea921c4006e6c7eb63c0
Phoebe Lau <phoebelau7@gmail.com>
2018-04-10 02:01:40 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function (){
 });
 
 function displayError() {
-  $("#errors").html("error")
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
 }
 
 function searchRepositories() {
@@ -11,12 +11,9 @@ function searchRepositories() {
   let url = `https://api.github.com/search/repositories?q=${searchTerms}`;
   $.get(url).done(function(response) {
     const results = response.items.map(r => { return (
-      `<strong><a href="${r.html_url}" target="_blank">${r.name}</a></strong><br/>
-      <em>${r.description}</em><br />
-      <small><a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></small>
-      <p></p>
-      <img src="${r.owner.avatar_url}" height="32" width="32"/><br />
-      <a href="${r.owner.url}" target="_blank">${r.owner.login}</a>
+      `<h2><a href="${r.html_url}">${r.name}</a></h2>
+      <p><a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      <p>${r.description}</p>
       <hr>`
     )})
     document.getElementById("results").innerHTML = results
@@ -31,10 +28,8 @@ function showCommits(el) {
   const url = `https://api.github.com/repos/${username}/${repo}/commits`
   $.get(url).done(function(response) {
     let commits = response.map(c => { return (
-      `${c.sha}<br />
-      ${c.commit.message}</p>
-      <img src="${c.author.avatar_url}" height="32" width="32"/><br/>
-      By ${c.commit.author.name} (<a href="${c.author.url}" target="_blank">${c.author.login}</a>)
+      `<h3>${c.sha}</h3>
+      <p>${c.commit.message}</p>
       <hr>`
     )});
     document.getElementById("details").innerHTML = commits
