4dd5c0d06822224205adb065d5b060ac4513bf91
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-04-18 02:57:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+function searchRepositories(){
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data =>{
+    $(`#results`).html(renderSearchResults(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function renderSearchResults(data){
+  data.items.map(result => renderSearchResult(result))
+}
+
+
 $(document).ready(function (){
 });
