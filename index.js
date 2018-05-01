7592881d8d4ee391403a44325e5ee53d5bfe15f9
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-01 16:02:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-$(document).ready(function (){
+
+  function displayError() {
+    $("#errors").html("I'm sorry, there's been an error. Please try again.");
+  }
+  
+  function renderCommit(commit) {
+    return `
+      <div>
+        <li><h3>${commit.sha}</h3><p>${commit.commit.message}<p></li>
+      <div> `  
+  }
+  
+  function renderCommits(data) {
+    let result = data.map((commit) => renderCommit(commit)).join('');
+    return `<ul>${result}</ul>`;
+  }
+  
+  var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+  
+  function searchRepositories() {
+    const searchTerms = $('#searchTerms').val();
+   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError();
+    });
+  }
+  
+  function renderSearchResult(result)  {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+  
+  function renderSearchResults(data) {
+    return data.items.map(result => renderSearchResult(result));
+  }
+  
+$(document).ready(function (){  
 });
