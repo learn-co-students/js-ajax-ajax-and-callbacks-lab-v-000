18b9adde6e34bedc34f1c196864edd2874eed6e0
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-10 18:07:45 +0000

    Done.

--- index.js
+++ index.js
@@ -11,11 +11,34 @@ function searchRepositories() {
         <li>${repo.name}</li>
         <li>${repo.description}</li>
         <li><a href="${repo.html_url}">Repo Link</a></li>
+        <li><a href="#" onclick="showCommits(this);" data-owner="${repo.owner.login}" data-repository="${repo.name}">Show Commits</a></li>
       </ul>
       `.trim();
       $("#results").append(resultsList);
     })
-  }).fail(function (error) {
-    $("#errors").append(`<p>Error, no repos found matching <em>${searchTerms}</em></p>`);
+  }).fail(displayError);
+}
+
+function displayError(error) {
+  $("#errors").append(`<p>error, no repos found matching <em>${searchTerms}</em></p>`);
+}
+
+function showCommits(link) {
+  const repo = link.dataset.repository;
+  const owner = link.dataset.owner;
+  const url = `https://api.github.com/repos/${owner}/${repo}/commits`
+  $.get(url).done(function (data) {
+    $("#details")[0].innerHTML = "";
+    data.forEach((result) => {
+      const commitList = `
+      <ul>
+        <li>${result.sha}</li>
+        <li>${result.commit.message}</li>
+        <li>${result.commit.author.name}</li>
+        <li>${result.author.login}</li>
+      </ul>
+      `.trim();
+      $("#details").append(commitList);
+    });
   });
 }
