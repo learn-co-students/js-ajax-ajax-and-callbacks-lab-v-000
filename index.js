84798c5062dfe8d9682c38191990663c21e9845b
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-10 15:17:06 +0000

    working

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-$(document).ready(function (){
-});
+function searchRepositories() {
+  const searchTerms = $("#searchTerms")[0].value;
+  const url = `https://api.github.com/search/repositories?q=${searchTerms}`;
+  $.get(url).done(function(data) {
+    $("#results")[0].innerHTML = "";
+    data.items.forEach((repo) => {
+      const resultsList = `
+      <ul>
+        <img scr=${repo.owner.avatar_url} height="80" width="80">
+        <li>${repo.owner.login}</li>
+        <li>${repo.name}</li>
+        <li>${repo.description}</li>
+        <li><a href="${repo.html_url}">Repo Link</a></li>
+      </ul>
+      `.trim();
+      $("#results").append(resultsList);
+    })
+  }).fail(function (error) {
+    $("#errors").append(`<p>Error, no repos found matching <em>${searchTerms}</em></p>`);
+  });
+}
