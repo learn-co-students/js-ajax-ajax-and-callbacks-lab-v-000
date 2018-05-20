616ae007d8aba1fb1d6499bd36a59834a1f11705
jacobsilver2 <jacobsilver2@mac.com>
2018-05-20 11:47:21 -0400

    is this right

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ $(document).ready(function (){
 function searchRepositories() {
   const term = document.getElementById('searchTerms').value;
   console.log(term);
+  const url = `https://api.github.com/search/repositories?q=${term}`
+  $.get(url, function () {
+    
+  })
 }
 
 
