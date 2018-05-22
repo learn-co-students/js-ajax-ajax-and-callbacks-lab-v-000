38935d29e102d6188983674917a23e9ecc9544b1
github username <github email address>
2018-05-22 16:09:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+function searchRepositories() {
+  let term = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories/q=${term}`, function(data) {
+    console.log(data);
+    $('div#results').html(data);
+  }).fail(function(error) {
+    alert("Something went wrong " + error);
+  });
+}
+
 $(document).ready(function (){
+
 });
