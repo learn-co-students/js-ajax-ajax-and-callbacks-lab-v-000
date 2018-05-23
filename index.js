1b70785494f805ae817eb1ef7772a0bfe275437e
github username <github email address>
2018-05-23 15:45:59 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function searchRepositories() {
   $.get(`https://api.github.com/search/repositories?q=${term}`, data => {
     console.log(getItems(data));
     $("#results").html(getItems(data));
-  });
+  }).fail(error => {
+      displayError()
+    })
+;
 };
 
 function getItems(data) {
@@ -20,6 +23,27 @@ function formatItems(result) {
         <p>${result.description}</p>
       </div> `
 };
+
+function showCommits(el) {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(getCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function getCommits(data) {
+  let result = data.map((commit)=>formatCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+function formatCommit(commit) {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
 
 $(document).ready(function (){
 
