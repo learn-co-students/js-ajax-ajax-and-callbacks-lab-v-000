6013caad5ca20439f86e7392b9c15bae502ee07a
github username <github email address>
2018-05-23 15:41:27 -0400

    Done.

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 function searchRepositories() {
   let term = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${term}`, data => {
+    console.log(getItems(data));
     $("#results").html(getItems(data));
   });
 };
 
 function getItems(data) {
-  data.items.map(result => {
-    formatItems(result);
+  return data.items.map(result => {
+    return formatItems(result);
   });
 };
 
 function formatItems(result) {
-  return
-     `<div>
+  return `<div>
         <h2><a href="${result.html_url}">${result.name}</a></h2>
         <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
         <p>${result.description}</p>
