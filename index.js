e69fe2efc64b055ca49834077503198002da1559
ronald vilorio <soccer23415@gmail.com>
2018-05-29 20:06:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-$(document).ready(function (){
+$(document).ready(function (data){
+  $.get("index.html",function(response){
+    searchRepositories()
+  })
 });
+
+function searchRepositories(){
+  const repos = JSON.parse(this.responseText)
+  const repoList = repos.map(r => {return '<li><a href="' + r.html_url + '">' + r.name + '</a></li>'}).join('')
+  document.getElementById("results").innerHTML = repoList
+}
+
+function showCommits(){
+
+}
