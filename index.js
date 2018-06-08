f3449dfb9d3ce75c6628ea6bb90cf2513a4acabb
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-08 16:48:16 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-$(document).ready(function (){
-});
+function displayError() {
+  $('#errors').html('error')
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(data.items.map(result => result.name))
+  })
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    $('#details').html(data.map(commit => commit.sha))
+  })
+}
