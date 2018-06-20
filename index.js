dfdfc8fc51f2f61a4d3011f71295290737f7eac1
github username <github email address>
2018-06-20 15:27:27 -0400

    Query the GitHub search API for repositories and displays them.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-$(document).ready(function (){
-});
+function searchRepositories() {
+    $(document).ready(function() {
+        const q = $("#searchTerms").val();
+        console.log(q);
+        $.get(`https://api.github.com/search/repositories?q=${q}`, function(data) {
+
+            $("#results").html(data.items.map(r => renderData(r)));
+        });
+    });
+}
+
+function renderData(repo) {
+    return `
+        <h4>Repository Info</h2>
+        Name:${repo.name} </br>
+        Description:${repo.description}</br>
+        Url: <a href="#"> ${repo.html_url}</a>
+        <h4>Owner Info</h2>
+        Avatar: <img src="${repo.owner.avatar_url}"> </br>
+        Name: ${repo.owner.login} </br>
+        Url: <a href="#"> ${repo.owner.html_url}"</a> </br>
+        `
+}
