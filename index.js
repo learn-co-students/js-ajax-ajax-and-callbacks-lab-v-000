a6516f52b9ab9687f9b7f636998f550902d51ac1
badlychadly <chadmontoya21@yahoo.com>
2018-06-28 16:34:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+
+
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+
+
+
+function showCommits(el) {
+  
+}
+
+function searchRepositories() {
+  
+  const searchTerms = $("#searchTerms").val()
+  
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, response => {
+    $('#results').html()
+  })
+}
+
+
 $(document).ready(function (){
 });
