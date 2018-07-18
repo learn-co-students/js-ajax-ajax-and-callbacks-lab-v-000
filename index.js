6249f01a57b8c4c270748660bbc5127d61162698
Aspen James <aspenjames@tqca.org>
2018-07-18 06:48:38 -0700

    save work

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-$(document).ready(function (){
-});
+function searchRepositories() {
+  let searchTerms = $("#searchTerms")[0].value;
+  let template = Handlebars.compile($("#repository-template")[0].innerHTML);
+  console.log(searchTerms);
+  $.get(`https://api.github.com/search/repositories?q="${searchTerms}"`, function(response) {
+    $("#results").html(template(response["items"][0]["responseJSON"]));
+  });
+};
