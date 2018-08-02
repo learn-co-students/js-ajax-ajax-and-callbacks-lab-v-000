bd53daac2808974df7e9bb16f8829da99751a6b0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-02 22:53:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+function displayError(){
+  return $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function listCommits(results){
+  return results.map (commit =>{
+		return `<ul><li> ${commit.sha}</li><li> ${commit.commit.message} </li></ul>`})
+}
+
+function showRepositories(results) {
+  return results.items.map (repo =>{
+    return `<div><h4><a href="test">${repo.name}</a><h4><p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick = "showCommits(this)"> Show Commits</a></p><p>${repo.description}</p></div>`})
+}
+
+function searchRepositories(){
+  const searchTerms = $('#searchTerms').val()
+  let url = `https://api.github.com/search/repositories?q=${searchTerms}`
+  $.get(url, results =>{$("#results").html(showRepositories(results))}).fail(error => {displayError()})
+}
+
+function showCommits(commit){
+  var url = `https://api.github.com/repos/${commit.dataset.owner}/${commit.dataset.repository}/commits`
+  $.get(url, results =>{$("#details").html(listCommits(results))}).fail(error => {displayError()})
+}
+
 $(document).ready(function (){
 });
