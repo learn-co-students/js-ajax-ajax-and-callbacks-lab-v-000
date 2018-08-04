b29c1251fc2f2f32067918a06b01bc1ff29fa579
Ruth <github email address>
2018-08-04 10:04:30 -0400

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function searchRepositories() {
   const uri = `https://api.github.com/search/repositories?q=${searchTerms}`
   $.get(uri, function(data) {
     $("#results").html(data.items.map(item => {
-      return `<h2><a href="${item.html_url}"></a> - <a href="#" onclick="showCommits(this)">Show Commits</a>'</h2>`
+      return `<h2><a href="${item.html_url}"></a> - <a href="#" data-repo="${item.name}" onclick="showCommits(this)">Show Commits</a>'</h2>`
     })
     )
   }).fail(displayError())
@@ -20,9 +20,8 @@ function showCommits(el) {
   const name = el.dataset.repository
   const owner = el.dataset.owner
   const uri = `https://api.github.com/repos/${owner}/${name}/commits/`
-  debugger
   $.get(uri, function(data) {
-    $("#details").html(data.items.map(item => {
+    $("#details").html(data.map(item => {
       return `<h2><a href="${item.url}"></a></h2>`
     }))
   })
