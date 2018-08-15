7b43ff8247d8504e0c6f85d169a51f1d8ed9505c
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 21:17:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 $(document).ready(function (){
 });
+// https://api.github.com/search/repositories?
+
+function searchRepositories() {
+  var searchValue = document.getElementById("searchTerms").value
+  var url = "http://api.github.com/search/repositories?q=";
+  $.get((url+searchValue), function(data) {
+    console.log(data);
+    // $('#results').html(formatFunction(data));
+  })
+}
+
+// formatFunction(data) {
+//   return ``
+// }
+
+
+// function getRepositories() {
+//   const req = new XMLHttpRequest()
+//   req.addEventListener("load", showRepositories);
+//   req.open("GET", 'https://api.github.com/users/octocat/repos')
+//   req.send()
+// }
