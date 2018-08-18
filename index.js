f9aac5a84175f0d2d9083dfea76f2716ec1b2766
Maverick Tesini <mavericktesini@gmail.com>
2018-08-18 03:56:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 $(document).ready(function (){
 });
-// https://api.github.com/search/repositories?
 
 function searchRepositories() {
   var searchValue = document.getElementById("searchTerms").value
   var url = "http://api.github.com/search/repositories?q=";
   $.get((url+searchValue), function(data) {
-    console.log(data.items[0])
-    console.log(data.items[0].name)
     $('#results').append("Name: " + data.items[0].name); //test line
-    // $("#results").html(data.items[0].name);
 
     displayRepositories();
-  }).fail(function(error) {
-  // This is called when an error occurs
-  console.log('Something went wrong: ', error);
-  $('#errorMessage').html('Something went wrong.', error.statusText);
-});
+  }).fail(displayError(error));
 }
+
+function displayError(error) {
+$('#errors').html('Something went wrong.', error.statusText);
+};
 
 function displayRepositories() {
   $('#results').append("Name: "); //username?
@@ -26,9 +22,3 @@ function displayRepositories() {
 $( document ).ajaxSuccess(function( event, request, settings ) {
   $( "#details" ).append( "<li>Successful Request!</li>" );
 });
-
-// $.get( "test.php", function( data ) {
-//   $( "body" )
-//     .append( "Name: " + data.name ) // John
-//     .append( "Time: " + data.time ); //  2pm
-// }, "json" );
