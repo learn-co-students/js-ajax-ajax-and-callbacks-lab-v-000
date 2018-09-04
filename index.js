0370f88f292a62a27526c3b574b2d00062f95f67
tsyaeger <tsyaeger@gmail.com>
2018-09-04 15:12:32 -0700

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ function searchRepositories(){
     $.get(url, function(results){
         let repoList = results.items.map( repo => {
 
-            return `<h3>${repo.name} ${repo.owner.login}</h3>
-            <img src="${repo.owner.avatar_url}" height="30">
-            <p><a href="${repo.owner.url}">Owner page</a><p>
-            <p>${repo.description}</p>
-            <p><a href="${repo.html_url}">Repo Link</a></p>
-            <p><a href="#" onclick="showCommits(this);" data-owner="${repo.owner.login}" data-repository="${repo.name}">ShowCommits</a></p>`
+            return `<ul>
+                        <h3>${repo.name}</h3>
+                        <li>${repo.description}</li>
+                        <img src="${repo.owner.avatar_url}" height="30">
+                        <li><a href="${repo.owner.url}">${repo.owner.login}</a></li>
+                        <li><a href="#" onclick="showCommits(this);" data-owner="${repo.owner.login}" data-repository="${repo.name}">ShowCommits</a></li>
+                     </ul>`
         })
 
           $('#results').html(repoList)
@@ -35,11 +36,12 @@ function showCommits(el){
 
     $.get(url, function(results){
         let commitList = results.map( commit => {
-            
-            return `<h3>${commit.sha}</h3
-                    <p>${commit.commit.author.name}</p>
-                    <p>${commit.author.login}</p>
-                    <img src="${commit.author.avatar_url}" height="30">`
+
+            return `<ul>
+                        <li>${commit.commit.author.name} - ${commit.author.login}</li>
+                        <img src="${commit.author.avatar_url}" height="30">
+                        <li>${commit.sha}</li>
+                    </ul>`
         })
 
         $('#details').html(commitList)
@@ -56,12 +58,5 @@ function displayError(){
     $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-
-
- // Include repository name, description, and a link to the HTML 
- // Also include repository owner login, repository owner avatar as an image, and a link to the owner's profile page
-
-// GET /repos/:owner/:repo/commits
- // For each commit, list the SHA, the author, the author's login, and the author's avatar as an image.
 
 
