2a8f998483149e3a0bd2191cdc5a5f67b8f760f9
tsyaeger <tsyaeger@gmail.com>
2018-09-03 18:25:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 $(document).ready(function (){
 });
+
+
+
+searchRepositories() {
+	const repoSearch = "https://api.github.com/search/repositories?q="
+	let searchTerm = $('#searchTerm')
+	let url = repoSearch + searchTerm
+
+
+	$.get(url, function(response) {
+	    // Here we are getting the element on the page with the id of sentences and
+	    // inserting the response
+	    $('#details').html(response);
+	}).fail(function(error) {
+	    // This is called when an error occurs
+	    console.log('Something went wrong: ' + error.statusText);
+	});
+
+
+}
+
+
+
+// $.get('this_doesnt_exist.html', function(data) {
+//     // This will not be called because the .html file request doesn't exist
+//     doSomethingGood();
+// }).fail(function(error) {
+//     // This is called when an error occurs
+//     console.log('Something went wrong: ' + error.statusText);
+// });
+
+
+
