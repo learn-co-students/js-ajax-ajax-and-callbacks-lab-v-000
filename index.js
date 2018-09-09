25c84190b99e0e51843d72cebc97a0af6bffb0c3
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-09 16:10:54 -0400

    Add search input and button. Attach handler on document ready using jQuery

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 $(document).ready(function (){
+    $('#search').on("click", searchRepositories);
 });
+
+
+function searchRepositories(){
+    const query = $('#searchTerms').prop('value');
+    console.log(`Query: "${query}"`);
+};
