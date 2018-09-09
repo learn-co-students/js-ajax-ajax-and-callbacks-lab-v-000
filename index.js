9165812e5467e53db1714536e2e149b3a507cc71
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-09 16:44:04 -0400

    Implement error handler. Update mocha timeout to prevent timeout during testing

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ $(document).ready(function (){
 
 
 function searchRepositories(){
-    const query = $('#searchTerms').prop('value');
+    const query = $('#searchTerms').val();
     console.log(`Query: "${query}"`);
+
+    $.get(`https://api.github.com/search/repositories?q=${query}`)
+    .done(showMatchingRepositories)
+    .fail(displayError); 
 };
+
+// AJAX CALLBACKS
+function showMatchingRepositories(response){
+    console.log("Success!");
+    console.log(response);
+}
+
+function displayError(){
+    $('#errors').text("I'm sorry, there's been an error. Please try again.");
+}
