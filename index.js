041e84df3aed886a2da8fad68b74297cf3b23671
AubreyNeimeier <neimeier.1@osu.edu>
2018-09-11 14:37:44 -0700

    3/4 tests passing, with get and show commits working but not rendering

--- index.js
+++ index.js
@@ -13,21 +13,35 @@ function searchRepositories(){
 
 function showRepositories(response){
     const repos = response.items;
-    return repos.map(repo => `<li> Name: {repo.name} </li><br>
-            <li> Description: {repo.description} </li><br>
-            <li> Link to HTML URL: {repo.html_url} </li><br>
-            <li> Owner Login: {repo.owner.login} </li><br>
-            <li> Avatar: <img src=" {repo.owner.avatar_url} "> </li><br>
-            <li> Owner's Profile Page: {repo.owner.url} </li><br>`);
-;}
+    return repos.map(repo => `<span><h2> Name: ${repo.name}</h2><b>Avatar: </b><img src= ${repo.owner.avatar_url} height="80" width="80"> </li><br></span>
+             <b>Description:</b> ${repo.description} <br>
+             <b>Owner Login: </b>${repo.owner.login} <br>
+             <a href="${repo.owner.url}"> Profile </a><br>        
+             <a href="${repo.html_url}"> Repo </a> <br></span></hr>
+             <a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onClick="showCommits(this)"> Show Commits </a>`);
+};
 
-// function printDetails(repo){
-//     return `<li> Name: {repo.name} </li><br>
-//             <li> Description: {repo.description} </li><br>
-//             <li> Link to HTML URL: {repo.html_url} </li><br>
-//             <li> Owner Login: {repo.owner.login} </li><br>
-//             <li> Avatar: <img src=" {repo.owner.avatar_url} "> </li><br>
-//             <li> Owner's Profile Page: {repo.owner.url} </li><br>    
-//             `
-// };
 
+function showCommits(el){
+    const owner = el.dataset.owner
+    const repo = el.dataset.repository
+    const url = `https://api.github.com/repos/${owner}/${repo}/commits`
+    $.get(url, function(response){
+        $("#details").html(displayCommits(response))
+    });
+
+}
+
+function displayCommits(response){
+    const commits = response;
+    debugger;
+    return commits.map(com=>
+        `<ul>
+            <li>Sha: ${com.sha}</li>
+            <li> Author: ${com.commit.author.name} </li>
+             
+        </ul>`
+    );
+}
+
+//<li> Author's Login: ${com.author.login} </li>
