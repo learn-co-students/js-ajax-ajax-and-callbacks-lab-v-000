184aab88a795948bb1f88d9e551464e269823e08
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-12 18:47:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-function showCommits() {
-
+function displayCommits(ev) {
+  const commits = JSON.parse(this.responseText);
+  console.log("display commits",responseText);
+  const commitsList = `<ul>${commits
+     .map(
+       commitInfo =>
+         '<li><strong>' +
+         commitInfo.author.login + ' - ' +
+         commitInfo.commit.author.name +
+         '</strong> - ' +
+         commitInfo.commit.message +
+         '</li>'
+     )
+     .join('')}</ul>`;
+   document.getElementById('details').innerHTML = commitsList;
 }
 
+function showCommits(el) {
+  const repo = el.dataset.repo;
+  const user = el.dataset.user;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  console.log("Show commits for ",`https://api.github.com/repos/${user}/${repo}/commits`);
+  req.open('GET', `https://api.github.com/repos/${user}/${repo}/commits`);
+  req.send();
+}
 function handleSearchResults(response) {
   const repos = response.items;
   console.log ("Search Results", repos);
@@ -10,10 +32,9 @@ function handleSearchResults(response) {
          repo =>
            '<li>' +
            '<strong>' +  repo.name + '</strong> -' +
-           repo.description +
-           '<a href="' + repo.html_url + '">' + repo.html_url + '</a>' +
-  //         '<a href="#" data-name=' + repo.name + '" data-user="' + repo.user + '">Show Commits</a>' +
-           '<a href="#" onclick="showCommits()" data-name=' + repo.name + '">Show Commits</a>' +
+           repo.description + '<br>' +
+           '<a href="' + repo.html_url + '">' + repo.html_url + '</a>' + '<br>' +
+           '<a href="#" data-repo=' + repo.name + '" data-user="' + repo.owner.login + '">Show Commits</a>' +
            '</li>'
        )
        .join('')}</ul>`;
