85c59c17f2326a4922785a8f1be5c0087e6901ed
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-12 21:45:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-function displayCommits(ev) {
-  const commits = JSON.parse(this.responseText);
-  console.log("display commits",responseText);
+function displayError(error) {
+//    document.getElementById("errors").innerHTML = error.statusText;
+ document.getElementById("errors").innerHTML = "error";
+}
+
+function displayCommits(commits) {
+  // ToDo: commitInfo.author.login may not be avail...
   const commitsList = `<ul>${commits
-     .map(
-       commitInfo =>
-         '<li><strong>' +
-         commitInfo.author.login + ' - ' +
-         commitInfo.commit.author.name +
-         '</strong> - ' +
-         commitInfo.commit.message +
-         '</li>'
-     )
-     .join('')}</ul>`;
-   document.getElementById('details').innerHTML = commitsList;
+    .map(
+      commitInfo =>
+        '<li><strong>' +
+        commitInfo.author.login + ' - ' +
+        commitInfo.commit.author.name +
+        '</strong> - ' +
+        '<img src ="' + commitInfo.author.avatar_url + '">' +
+        commitInfo.sha +
+        '</li>'
+    )
+   .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
 
 function showCommits(el) {
-  const repo = el.dataset.repo;
-  const user = el.dataset.user;
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', displayCommits);
-  console.log("Show commits for ",`https://api.github.com/repos/${user}/${repo}/commits`);
-  req.open('GET', `https://api.github.com/repos/${user}/${repo}/commits`);
-  req.send();
+  const repo = el.dataset.repository;
+  const user = el.dataset.owner;
+  $.get(`https://api.github.com/repos/${user}/${repo}/commits`, displayCommits).fail(displayError);
 }
 function handleSearchResults(response) {
   const repos = response.items;
-  console.log ("Search Results", repos);
   const repoList = `<ul>${repos
        .map(
          repo =>
@@ -34,7 +34,7 @@ function handleSearchResults(response) {
            '<strong>' +  repo.name + '</strong> -' +
            repo.description + '<br>' +
            '<a href="' + repo.html_url + '">' + repo.html_url + '</a>' + '<br>' +
-           '<a href="#" data-repo=' + repo.name + '" data-user="' + repo.owner.login + '">Show Commits</a>' +
+           '<a href="#" onclick="showCommits(this)" data-repository="' + repo.name + '" data-owner="' + repo.owner.login + '">Show Commits</a>' +
            '</li>'
        )
        .join('')}</ul>`;
@@ -48,7 +48,7 @@ function searchRepositories () {
   let searchTerms = document.getElementsByName("searchTerms")[0].value;
   // ex. https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
   let uri="https://api.github.com/search/repositories?q="+searchTerms;
-  $.get(uri, handleSearchResults);
+  $.get(uri, handleSearchResults).fail(displayError);
 }
 
 $(document).ready(function (){
