d6d26afac5d908fddf09f65d10438ca329ceedd0
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-12 17:47:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+function showCommits() {
+
+}
+
 function handleSearchResults(response) {
-  console.log("Search Results",response);
   const repos = response.items;
-  console.log("repos",repos)
-  // const repoList = `<ul>${repos
-  //      .map(
-  //        repo =>
-  //          '<li>' +
-  //          '<strong>' +  repo.name + '</strong> -' +
-  //          repo.description +
-  //          '<a href="' + repo.html_url + '">' + repo.html_url + '</a>' +
-  //          '</li>'
-  //      )
-  //      .join('')}</ul>`;
-    const src = document.getElementById('repository-template').innerHTML;
-    const template = Handlebars.compile(src);
-    const repoList = template(repos);
+  console.log ("Search Results", repos);
+  const repoList = `<ul>${repos
+       .map(
+         repo =>
+           '<li>' +
+           '<strong>' +  repo.name + '</strong> -' +
+           repo.description +
+           '<a href="' + repo.html_url + '">' + repo.html_url + '</a>' +
+  //         '<a href="#" data-name=' + repo.name + '" data-user="' + repo.user + '">Show Commits</a>' +
+           '<a href="#" onclick="showCommits()" data-name=' + repo.name + '">Show Commits</a>' +
+           '</li>'
+       )
+       .join('')}</ul>`;
+    // const src = document.getElementById('repository-template').innerHTML;
+    // const template = Handlebars.compile(src);
+    // const repoList = template(repos);
     document.getElementById('results').innerHTML = repoList;
   }
 
