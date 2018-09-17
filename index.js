7c9c60ca9dccf839fa30be25f8e6e7fd16eb26d8
Dan Suarez <suarez.daniel101@gmail.com>
2018-09-17 22:47:06 +0000

    Done.

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function displayRepos(response) {
 
 function displayError(error) {
   //debugger
-  error = "I'm sorry, there's been an error. Please try again."
+  error = "I'm sorry, there's been an error. Please try again." + error
   $('#errors').html(error);
   //document.getElementById('errors').innerHTML = error;
 };
