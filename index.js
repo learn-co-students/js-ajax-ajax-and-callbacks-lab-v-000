55692fe6439309094860466d2644d63b592a2350
cbshields <cbshields@outlook.com>
2018-09-19 17:03:33 -0700

    started search repositories

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 $(document).ready(function (){
 });
+
+let searchterm = document.getElementbyId('searchTerms').value
+$.get(`https://api.github.com/search/repositories?q=${searchterms}`, data => {$("#results").html(
+
+
+)}
