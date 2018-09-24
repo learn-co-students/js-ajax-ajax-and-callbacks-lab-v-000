d4d15ceea1c81bb3b044ac47d5ee018b37d112a0
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-24 17:25:30 +0000

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ $(document).ready(function (){
       console.log(repoData)
       $('#results').html(
         repoData.items.map(function(repo) {
-          return `<div style="display:flex"><img src="${repo.owner.avatar_url}", style="max-width:100px;max-height:100px;float:left;"><p style="padding-left:15px"><a href="${repo.html_url}">${repo.name}</a> - ${repo.description} - <a href="${repo.owner.html_url}">${repo.owner.login}</a></p></div>` 
+          return `<div style="display:flex"><img src="${repo.owner.avatar_url}", style="max-width:100px;max-height:100px;float:left;"><p style="padding-left:15px"><a href="${repo.html_url}">${repo.name}</a> - ${repo.description} - <a href="${repo.owner.html_url}">${repo.owner.login}</a> - <a href="#" onclick="showCommits(this)">Show Commits</a></p></div>` 
         })
         );
 	  });
+  }
+  
+  function showCommits(commits) {
+    
   }
   
