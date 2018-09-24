b20cbdd200abf97f1f89faa6ca1a1e2da3fbd935
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-24 16:41:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 $(document).ready(function (){
+
+  
 });
+  
+  function searchRepositories() {
+    let terms = $('#searchTerms').val()
+    $.get(`https://api.github.com/search/repositories?q=${terms}`, function(repoData) {
+      console.log(repoData)
+      $('#results').html(
+        repoData.items.map(function(repo) {
+          return `<p><a href="${repo.html_url}">${repo.name}</a> - ${repo.description}</p>` 
+        })
+        );
+	  });
+  }
+  
