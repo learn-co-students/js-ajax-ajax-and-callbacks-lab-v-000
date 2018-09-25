c93606a5756f08188d9e13366b5b31a043887766
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 22:49:10 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ $(document).ready(function (){
 
 });
 
+function buildHTML(response){
+  let responseHTML;
+
+
+  $('#results').html(responseHTML);
+}
+
 function searchRepositories() {
   let searchText = document.getElementById('searchTerms').value;
   let regex = /\s/
@@ -12,8 +19,19 @@ function searchRepositories() {
   console.log("search URl", searchUrl )
 
   $.get(searchUrl, function(response){
-    // $('#results').html(response);
+    $('#results').html(response);
+
+    // Display the collection of repositories inside the results div. Include repository name, description, and a link to the HTML URL. Also include repository owner login,
+    // repository owner avatar as an image, and a link to the owners profile page. Hint: Pay close attention to the structure of the search results!
     console.log("results", response);
   });
+
+}
+
+function showCommits() {
+
+}
+
+function displayError(){
 
 }
