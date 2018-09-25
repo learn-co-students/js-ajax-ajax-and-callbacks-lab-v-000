50079903ec43c5485c90ebda85bf01a2cea2c112
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 23:20:59 +0000

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ $(document).ready(function (){
 
 });
 
-function buildHTML(response){
-  let responseHTML;
-
-
-  $('#results').html(responseHTML);
-}
+// function buildRepoHTML(response){
+// }
 
 function searchRepositories() {
   let searchText = document.getElementById('searchTerms').value;
@@ -19,19 +15,30 @@ function searchRepositories() {
   console.log("search URl", searchUrl )
 
   $.get(searchUrl, function(response){
-    $('#results').html(response);
+    let repos = response.items;
+    console.log("repos", repos)
+    let repoList = `<ul>${repos.map(r =>
+      '<li>' +
+      r.name + ' - ' +
+      r.description + ' - ' +
+      `<a href="${r.html_url}">${r.html_url}</a>` + ' - ' +
+      r.owner.login + ' - ' +
+      `<img src="${r.owner.avatar_url}">` +
+      `<a href="${r.owner.html_url}">${r.owner.html_url}</a>` + ' - ' +
+      '</li>').join('')}</ul>`
 
-    // Display the collection of repositories inside the results div. Include repository name, description, and a link to the HTML URL. Also include repository owner login,
-    // repository owner avatar as an image, and a link to the owners profile page. Hint: Pay close attention to the structure of the search results!
-    console.log("results", response);
-  });
+// include repository owner login, repository owner avatar as an image, and a link to the owner's profile page.
+// Hint: Pay close attention to the structure of the search results!
 
+    $('#results').html(repoList);
+  })
 }
 
 function showCommits() {
 
 }
-
-function displayError(){
-
-}
+//
+// function displayError(error){
+//     let errorHTML = `<p>${error}</p>`
+//     $('#errors').html.(errorHTML);
+// }
