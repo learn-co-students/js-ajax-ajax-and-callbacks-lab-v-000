a5a790ad0fc70cda0a5c600532d072c8a4d6848c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-25 15:56:33 +0000

    Done.

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ $(document).ready(function (){
       console.log(repoData)
       $('#results').html(
         repoData.items.map(function(repo) {
-          return `<div style="display:flex"><img src="${repo.owner.avatar_url}", style="max-width:100px;max-height:100px;float:left;"><p style="padding-left:15px"><a href="${repo.html_url}">${repo.name}</a> - ${repo.description} - <a href="${repo.owner.html_url}">${repo.owner.login}</a> - <a href="#" onclick="showCommits(this)">Show Commits</a></p></div>` 
+          return `<div style="display:flex"><img src="${repo.owner.avatar_url}", style="max-width:100px;max-height:100px;float:left;"><p style="padding-left:15px"><a href="${repo.html_url}">${repo.name}</a> - ${repo.description} - <a href="${repo.owner.html_url}">${repo.owner.login}</a> - <a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a></p></div>` 
         })
         );
 	  });
   }
   
-  function showCommits(commits) {
-    
+  function showCommits(repository) {
+   let repoName = repository.dataset.repository;
+   let repoOwner = repository.dataset.owner;
+   console.log(repoName)
+   console.log(repoOwner)
+    $.get(`https://api.github.com/repos/${repoOwner}/${repoName}/commits`, function(repoData) {
+      console.log(repoData)
+        $('#results').html(repoData)
+    })
   }
   
