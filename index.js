46258961c04d9b11e1dcf7e2789a4a352de8b937
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 22:44:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 $(document).ready(function (){
+
+
+
 });
+
+function searchRepositories() {
+  let searchText = document.getElementById('searchTerms').value;
+  let regex = /\s/
+  let searchConverted = searchText.replace(regex, "+");
+  let searchUrl = "https://api.github.com/search/repositories?q=" + searchConverted;
+  console.log("search URl", searchUrl )
+
+  $.get(searchUrl, function(response){
+    // $('#results').html(response);
+    console.log("results", response);
+  });
+
+}
