e36bea8874f6d77b3e8b908483c1a9b5fd368f28
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-10-12 18:31:29 -0400

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ function searchRepositories() {
     let term = $('#searchTerms').val();
     $.get(`https://api.github.com/search/repositories?q=${term}`, function(data){
         showRepositories(data);
-    }).fail(displayError());
+    });
 };
 
 function displayError() {
     $('#errors').html("An error occurred! Please try again!")
 };
 
-function showCommits(repo) {
-    console.log(repo);  
-    let url = repo.dataset.commits.slice(0,-6).toString(); //getting rid of the {/sha}
-    $.get(url, function(response){
+function showCommits(el) {
+    // let url = repo.dataset.commits.slice(0,-6).toString(); //getting rid of the {/sha}
+    $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(response){
         console.log(response);
         displayCommits(response);
     }).fail(displayError());
@@ -26,13 +25,15 @@ function showRepositories(data) {
     $('#results').html(data.items.map(
         i => {return `<div><li>${i.name}</li>
         <p>${i.description}</p>
-        <a href='#' data-commits='${i.commits_url}' onclick='showCommits(this)'>Show commits</a></div></br>`})
+        <a href='#' data-repository='${i.name}' data-owner='${i.owner.login}' onclick='showCommits(this)'>Show commits</a></div></br>`})
         );
 };
 
 function displayCommits(data) {
     $('#details').html(data.map(
-        c => {return `<div><li>${c.sha}</li>
-        <p>${c.commit.author.name}</p></div>`})
-        );
+        c => {if (c.author !== null) return `<div><li>${c.sha}</li>
+        <p>${c.author.login}</p>
+        <p>${c.commit.author.name}</p>
+        <img src="${c.author.avatar_url}" width='50'></div>`})
+        );  
 };
