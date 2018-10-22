6494bd528d9f5992b41966a1fedc2da7e2ca631b
Sara Stanton <sara.mpa13@gmail.com>
2018-10-22 02:16:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,77 +1,33 @@
-// $(document).ready(function (){
+function searchRepositories() {
+  const search = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${search}`, function(data) {
+    $('#results').html(repoTemplate(data));
+  }).fail(error => displayError());
+}
 
-  function searchRepositories() {
-    const input = document.querySelector('input#searchTerms').value;
-    const req = new XMLHttpRequest();
-    req.addEventListener('load', displayRepositories);
-    req.open('GET', 'https://api.github.com/search/repositories?q=' + input)
-    // req.open('GET', 'https://doesnotexist.com' + input)
 
-    req.send();
-  }
+function repoTemplate(data) {
+  return data.items.map(result => `
+  <div>
+    <a href="${result.html_url}">${result.name}</a>
+    <a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a>
+    <p>${result.description}</p>
+  </div>
+  `);
+}
 
-  function displayRepositories() {
-    const repos = JSON.parse(this.responseText);
-    console.log(repos);
-    const repoList = `<ul>${repos.items
-      .map(
-        r =>
-            '<li>' +
-              '<p>' +
-              '<img src="' + r.owner.avatar_url + '" alt="' + r.owner.login + '" width="50" height="50" />' +
-              '</p>' +
-              '<p><strong> Name: </strong>' +
-              '<a href="' + r.html_url + '">' + r.name + '</a>' +
-              '</p>' +
-              //ADD LINK
-              '<p><strong> Description: </strong>' +
-              r.description +
-              '</p>' +
-              '<p><strong> Owner (username): </strong>' +
-              '<a href="https://github.com/' + r.owner.login + '">' + r.owner.login + '</a>' +
-              '</p>' +
-              '<a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)"> Get Commits </a>' +
-            '</li>'
-            )
-          .join('')}</ul>`;
-          document.getElementById('results').innerHTML = repoList;
-  }
+function showCommits(repo) {
+  $.get(`https://api.github.com/repos/${repo.dataset.owner}/${repo.dataset.repository}/commits`, function(data) {
+    $('#details').html(commitTemplate(data));
+  }).fail(error => displayError());
+};
 
-  function getCommits(el) {
-    const name = el.dataset.repository;
-    const user = el.dataset.username;
-    const req = new XMLHttpRequest();
-    req.addEventListener('load', showCommits);
-    req.open('GET', 'https://api.github.com/repos/' + user + '/' + name + '/commits');
-    req.send();
-  }
+function commitTemplate(data) {
+  return data.map(commit => `
+  <li>${commit.sha} - ${commit.author.login} - ${commit.commit.message}</li>
+  `)
+}
 
-  function showCommits() {
-    const commits = JSON.parse(this.responseText);
-    const commitsList = `<ul>${commits
-      .map(
-        commit =>
-        '<li>'+
-          '<img src="' + commit.author.avatar_url + '" alt="' + commit.commit.author.name + '" width="50" height="50" />' +
-          '<p><strong> SHA: ' +
-          commit.commit.tree.sha +
-          '</p>' +
-          '<p><strong> Author: ' +
-          commit.commit.author.name +
-          '</p>' +
-          '<p><strong> Username: ' +
-          '<a href="https://github.com/' + commit.author.login + '">' + commit.author.login + '</a>' +
-          '</p>' +
-        '</li>'
-      ).join('')}</ul>`;
-      document.getElementById('details').innerHTML = commitsList;
-  }
-  function displayErrors() {
-    document.getElementById('errors').innerHTML = "I'm sorry, there's been an error. Please try again.";
-  }
-
-  if(searchRepositories() === undefined) {
-      displayErrors();
-  };
-
-// })
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
