ae43b523d33ee5d15503660a26bd8e8a66a7a93f
sgibson47 <sgibson47@gmail.com>
2018-10-23 09:33:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 $(document).ready(function (){
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
 });
+
+
+
+function displayError(){
+  document.getElementById('errors').innerHTML = "I'm sorry, there's been an error. Please try again."
+}
+
+//GET /search/repositories
+// q string  Required. The search keywords, as well as any qualifiers.
+// /search/repositories?q=tetris
+function searchRepositories() {
+  const searchTerms = document.getElementById("searchTerms").value
+  const searchTermsURL = "https://api.github.com" + "/search/repositories?q=" + searchTerms
+  $.get(searchTermsURL).done(function(response){
+    console.log(response.items)
+    const src = document.getElementById('repository-template').innerHTML;
+    const template = Handlebars.compile(src);
+    const repoList = template(response.items);
+    document.getElementById('results').innerHTML = repoList;
+  }).fail(displayError);
+}
+
+
+// GET /repos/:username/:repo/commits
+function showCommits(element){
+  const name = element.dataset.name;
+  const owner = element.dataset.owner;
+  const repoURL = "https://api.github.com" +'/repos/'+owner+'/'+name+'/commits'
+  $.get(repoURL).done(function(response){
+    console.log(response)
+    const src = document.getElementById('commits-template').innerHTML;
+    const template = Handlebars.compile(src);
+    const commitsList = template(response);
+    document.getElementById('details').innerHTML = commitsList;
+  }).fail(displayError);
+}
