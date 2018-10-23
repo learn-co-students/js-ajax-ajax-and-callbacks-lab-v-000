a7f126f59c84bf7cc5b869a068056b7ad5a1808e
sgibson47 <sgibson47@gmail.com>
2018-10-23 11:06:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
-$(document).ready(function (){
-  Handlebars.registerPartial(
-    'authorPartial',
-    document.getElementById('author-partial-template').innerHTML
-  );
-});
-
-
-
 function displayError(){
-  document.getElementById('errors').innerHTML = "I'm sorry, there's been an error. Please try again."
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
+
+function commitListItem(commit){
+  return `<li>
+    <p>SHA: ${commit.sha}</p>
+    <section><header><h4>Created By ${commit.commit.author.name}</h4></header>
+    <p>Login: ${commit.committer.login}</p>
+    <img src="${commit.committer.avatar_url}" height="32" width="32"></section>   
+  </li>`
+}
+
+function commitListItems(response){
+  let commitListItemsString = response.map( (commit) => commitListItem(commit)).join('')
+  return commitListItemsString
+}
+
+function ownerDetails(repo){
+  return `<header><h4>Created By ${repo.owner.login}</h4></header>
+  <img src="${repo.owner.avatar_url}" height="32" width="32">`
+}
+
+function repoListItem(repo){
+  return `<li>
+     <h2><a href="${repo.html_url}">${repo.name}</a></h2>
+     <section>${ownerDetails(repo)}</section>
+     <p>Description: ${repo.description}</p>
+     <a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a>
+   </li>`
+}
+
+function repoListItems(response){
+  let repos = response.items
+  let repoListItemsString = repos.map( (repo) => repoListItem(repo)).join('')
+  return repoListItemsString
 }
 
 //GET /search/repositories
 // q string  Required. The search keywords, as well as any qualifiers.
 // /search/repositories?q=tetris
 function searchRepositories() {
-  const searchTerms = document.getElementById("searchTerms").value
+  const searchTerms = $("#searchTerms").val();
   const searchTermsURL = "https://api.github.com" + "/search/repositories?q=" + searchTerms
   $.get(searchTermsURL).done(function(response){
     console.log(response.items)
-    const src = document.getElementById('repository-template').innerHTML;
-    const template = Handlebars.compile(src);
-    const repoList = template(response.items);
-    document.getElementById('results').innerHTML = repoList;
+    $('#results').html('<ul>'+ repoListItems(response)+ '</ul>');
   }).fail(displayError);
 }
 
 
 // GET /repos/:username/:repo/commits
 function showCommits(element){
-  const name = element.dataset.name;
-  const owner = element.dataset.owner;
-  const repoURL = "https://api.github.com" +'/repos/'+owner+'/'+name+'/commits'
+  const repoURL = `https://api.github.com/repos/${element.dataset.owner}/${element.dataset.repository}/commits`
   $.get(repoURL).done(function(response){
     console.log(response)
-    const src = document.getElementById('commits-template').innerHTML;
-    const template = Handlebars.compile(src);
-    const commitsList = template(response);
-    document.getElementById('details').innerHTML = commitsList;
+    $('#details').html('<ul>'+ commitListItems(response)+ '</ul>');
   }).fail(displayError);
 }
