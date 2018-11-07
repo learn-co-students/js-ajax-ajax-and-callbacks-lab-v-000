3a6330e9e7f920f899853f08eee2da5936d3d135
github username <github email address>
2018-11-06 20:42:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+function displayError() {
+  // The tests wants a Jquery call from $('#errors').html with a phrase in quotes that has error in it
+  $('#errors').html('error')
+}
+
+function searchRepositories() {
+  $.get('#searchTerms').val
+}
+
 $(document).ready(function (){
 });
