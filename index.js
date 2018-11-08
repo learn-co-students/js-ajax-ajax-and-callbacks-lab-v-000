00d29891ab63927bfe4d47dd8bd9a671baf2f525
github username <github email address>
2018-11-08 06:44:42 -0500

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,49 @@ function displayError() {
   $('#errors').html('error')
 }
 
-function searchRepositories() {
-  $.get('#searchTerms').val
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+var showCommits = (el) => {
+  // Load data from the server using a HTTP GET Request to grab the github api from the specific Repo Owner, their repository, and their commits
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+var renderSearchResult = (result) => {
+  // Print outs the repositories API information with the specific data we want.
+  debugger;
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+// Iterate through the data set to print out the respository data
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  // Load data from the server using a HTTP GET Request with the specific searchTerm
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    // Using a callback function here to use show the search results
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
 }
 
 $(document).ready(function (){
