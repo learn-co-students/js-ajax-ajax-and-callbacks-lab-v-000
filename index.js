a27beca918a99ab5f62a7299e74b84842fc720a2
roitblatari <roitblatari@gmail.com>
2018-11-08 19:43:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
+function displayError (){
+  $('#errors').html('There is a error: ')
+}
 
-var searchRepositories = () => {
-  let searchWord = $('#searchTerms').val()
+function searchRepositories (){
+  const searchWord = $('#searchTerms').val()
   // console.log($('#results').html());
   $.get(`https://api.github.com/search/repositories?q=${searchWord}`,repositories => {
     $('#results').html(renderSearchResults(repositories) )
@@ -9,53 +12,45 @@ var searchRepositories = () => {
   } )
 }
 
-var renderSearchResults = (r) => { r.items.map(rep => renderSearchResult(rep) ) }.fail(function(error) {
-    // This is called when an error occurs
-    console.log('Something went wrong ari: ' + error.statusText);
-});
-
+var renderSearchResults = (r) => {
+  return r.items.map(rep => renderSearchResult(rep) );
+}
 
 var renderSearchResult = (r) => {
-  // return data.items.map( r => `
   return `
   <div>
    <h2><a href="${r.html_url}">${r.name}</a> </h2>
     <p>  <a href="${r.owner.html_url}">${r.owner.login}</a>  </p>
     <img src="${r.owner.avatar_url}" alt="">
     <p> ${r.description} </p>
-    <a onclick="${showCommits(this)}">Show Commits</a>
+    <a href="#" data-info="${r.commits_url}" data-repository="${r.name}" data-owner="${r.owner.login}"  onclick="showCommits(this)">Show Commits</a>
   </div>
     `;
   // );
 }
 //  For each commit, (list the SHA), the author, the author's login, and the author's avatar as an image
 var renderCommits = (dataCommits) => {
-//   // dataCommits
-//   dataCommits.map(c => {`
-//     <p>  ${c.sha}</p>
-//     <p>  ${c.commit.author.name}</p>
-//   `})
-// debugger
+  return  dataCommits.map(c => renderCommit(c))
 }
-// r.commits_url
-// "https://api.github.com/repos/dankerizer/searchterms-tagging-2/commits{/sha}"
+var renderCommit = (c) => {
+  return `
+    <p>  ${c.sha}</p>
+    <p>  ${c.commit.author.name}</p>
+  `
+}
+
+
 var showCommits = (r) => {
-  let commits = r.commits_url
-  // debugger
-  commits = commits.replace(/commits.+/,"commits")
-  $.get(commits, commitsList => {
+  let urlCommits = `https://api.github.com/repos/${r.dataset.owner}/${r.dataset.repository}/commits`
+  $.get(urlCommits, commitsList => {
     $("#details").html(renderCommits(commitsList) )
+  }).fail(error => {
+    displayError()
   })
-
-  // $.get(commits_url)
 }
 
 
-var displayError = () => {
-  $('#errors').html('There is a error: ')
-}
 
 $(document).ready(function (){
-  // searchRepositories()
-  // showCommits()
+  searchRepositories()
 });
