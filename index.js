6202a716bfc303f8afd24c511b4d7f565d2a0aa2
ElleeB <laura.trager@gmail.com>
2018-11-26 12:29:27 -0500

    Add searchRepositories fx

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 $(document).ready(function (){
-});
+
+})
+
+function searchRepositories() {
+  let searchTerms = $("#searchTerms")[0].value
+  const github = "https://api.github.com"
+  const url = github + "/search/repositories" + `?q=${searchTerms}`
+  $.get(url).done(function(data) {
+    displayRepositories(data)
+  })
+}
+
+function displayRepositories(data) {
+  // $('#results').html(info)
+  console.log("we're here!!!")
+  console.log(data)
+
+  // console.log(data)
+  // console.log(data.items[0].name)
+}
