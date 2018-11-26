e8b00e8bed8ba309c5c2f265ffb9058181bae03b
anikonchuk <anikonch@gmail.com>
2018-11-26 13:49:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 $(document).ready(function (){
 });
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
+
+function searchRepositories () {
+  let searchTerms = document.getElementById('searchTerms').value;
+  $.get(`https://api.github.com/search/repositories?q=#{searchTerms}`, function(data) {
+    displayRepositories();
+  }).fail(function(error){
+    displayError();
+  })
+}
