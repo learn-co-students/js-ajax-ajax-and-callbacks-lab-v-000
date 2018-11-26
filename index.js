a8033790c89a45154d8262b48c6780f7d952a633
ElleeB <laura.trager@gmail.com>
2018-11-26 15:14:48 -0500

    Passing all but commits tests

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ $(document).ready(function (){
 })
 
 function searchRepositories() {
-  let searchTerms = $("#searchTerms")[0].value
-  const github = "https://api.github.com"
-  const url = github + "/search/repositories" + `?q=${searchTerms}`
-  $.get(url).done(function(data) {
-    displayRepositories(data)
+  let formatRepo = (repo) => {
+    return `
+      <div>
+        <h3><a href="${repo.html_url}"> ${repo.name} </a></h3>
+          <p>${repo.description}</p>
+          <h4>${repo.owner.login}</h4>
+          <h5><a href="${repo.owner.html_url}">${repo.owner.html_url}</a></h5>
+          <img src="${repo.owner.avatar_url}" height="80px"><br></li>
+      </div>
+      <hr>
+      `
+  }
+
+  let formatResults = (data) => data.items.map(repo => formatRepo(repo))
+
+  let searchTerms = $("#searchTerms").val()
+  let url = "https://api.github.com" + "/search/repositories" + `?q=${searchTerms}`
+
+  $.get(url, function(data) {
+    $('#results').html(formatResults(data))
   })
-}
-
-function displayRepositories(data) {
-  // $('#results').html(info)
-  console.log("we're here!!!")
-  console.log(data)
-
-  // console.log(data)
-  // console.log(data.items[0].name)
 }
