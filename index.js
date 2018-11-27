76ea8082d43327c8797d8f1aeb3b49537dfe8080
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-27 18:51:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+$(button).on('click', function(){
+  console.log(data)
+})
+
+$.get({
+  url : 'https://api.github.com/repositories',
+  type : 'GET'
+  })
+.done(function(data) {
+  console.log(data)
+})
+
 $(document).ready(function (){
 });
