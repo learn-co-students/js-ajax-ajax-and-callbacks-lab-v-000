68ba0aec536e2c45899bed9bf9fcc52ef4ed96d3
github username <github email address>
2018-12-03 09:27:35 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+const renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+const renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+function showCommits(el) {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+const renderSearchResult = (result) => {
+  return `
+    <div>
+      <h2><a href="${result.html_url}">${result.name}</a></h2>
+      <img src="${result.owner.avatar_url}" height="32" width="32">
+      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      <p>${result.description}</p>
+    </div>
+    <hr>
+  `
+}
+
+const renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(renderSearchResults(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
 $(document).ready(function (){
 });
