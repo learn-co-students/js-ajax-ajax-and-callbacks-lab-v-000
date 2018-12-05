8cff4b658af4da0b1dd9614f60f0b0b320fc02ca
Anu Khambete <anukhambete@gmail.com>
2018-12-05 20:36:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 $(document).ready(function (){
+
+
+
 });
+
+function displayError() {
+  document.getElementById('errors').innerHTML = "error";
+}
+
+function searchRepositories() {
+  //debugger
+  const term = document.getElementById('searchTerms').value
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/search/repositories?q=' + term);
+  req.send();
+}
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText).items;
+  debugger
+  const repoList = `<ul>${repos
+    .map(r => '<li><a href=' + r.html_url + '>' + r.name + '-' + r.description + '</a></li>')
+    .join('')}</ul>`;
+  document.getElementById('results').innerHTML = repoList;
+  //debugger
+}
