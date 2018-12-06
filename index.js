a29cb874259abef2e9570eeeae09547b892fa629
stazman <cadistasio@gmail.com>
2018-12-06 04:05:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+
+
+
+let searchRepositories = () => {
+    const searchTerms = $('#searchTerms').val()
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+        $('#results').html(renderSearchResults(data))
+      }).fail(error => {
+        displayError()
+      })
+}
+  
+
 $(document).ready(function (){
 });
 
-function searchRepositories() {
-    
-}
+// $.get('results', function(respnoses))
