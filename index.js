fc10d41713a4e8ab403ea2ebb874d496a45b813d
github username <github email address>
2018-12-09 16:54:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
 
-$(document).ready(function (){
-});
 
+function displayError() {
+  const error = document.getElementById('errors').value
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
 
-  function displayError() {
-    $('#errors').html("I'm sorry, there's been an error. Please try again.")
-  }
+function displayResponse(response) {
+  let result = response.items.map(repo => {
+  return `
+    <li>
+    <h3><a href="${repo.html_url}">${repo.name} </a></h3>
+     <p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}"
+     onclick=showCommits(this);return false;"> Show Commits </a></p>
+    <p>${repo.description}</p>
+    </li>
+    `
 
-  function displayResponse(response) {
-    let result = response.items.map(repo => {
-		return `
-			<li>
-			<h3><a href="${repo.html_url}">${repo.name}</a></h3>
-			<p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}"
-			onclick="showCommits(this);return false;">Show Commits</a></p>
-			<p>${repo.description}</p>
-			</li>
-			`
-    })
-    $('#results').html(result);
+})
+  $('#results').html(result);
 
-  }
+}
 
-  function showCommits(el) {
-  	const repoName = el.dataset.repository;
-  	const owner = el.dataset.owner;
-  	$.get(`https://api.github.com/repos/${owner}/${repoName}/commits`,
-  		function(response) {
-  			$('#details').html(displayCommits(response));
-  		}).fail(displayError)
-  	}
-
-    function displayCommits(result) {
-    	const commitList = "<ul>" + result.map(commit => {
-    		return `
-    			<li>
-    			<h3>SHA: ${commit.sha}</h3>
-    			<h2>Author: ${commit.commit.author.name}</h2>
-    			<p>Author Login: ${commit.committer.login }</p>
-    			<img src="${commit.committer.avatar_url}" height=50px width = 50px />
-    			</li>
-    			`
-    	}).join('') + "</ul>";
-    	return commitList;
+function showCommits(el) {
+   const repoName = el.dataset.repository;
+   const owner = el.dataset.owner;
+   $.get(`https://api.github.com/repos/${owner}/${repoName}/commits`,
+     function(response) {
+       $('#details').html(displayCommits(response));
+     }).fail(displayError)
    }
 
+function displayCommits(response) {
+  console.log(response)
+  let commits = response.map(commit => {
+    return `
+    <li>
+    <h3>${commit.commit.author.name}</h3>
+    <p>${commit.sha}</p>
+    <p>${commit.committer.login}</p>
+    <p> ${commit.committer.avatar_url}</p>
+    </li>
+    `
+  })
+  $('#details').html(commits)
+}
 
-  function searchRepositories() {
-      let search = document.getElementById("searchTerms").value;
-
-      $.get(`https://api.github.com/search/repositories?q=${search}`, displayResponse).fail(displayError)
-  }
+function searchRepositories(){
+  let searchTerm = document.getElementById('searchTerms').value
+  $.get(`https://api.github.com/search/repositories?q=${searchTerm}`, displayResponse).fail(displayError)
+}
