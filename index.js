047eb3893e4e1b298c14b9775d7100008a629f9e
Christopher DiStasio <cadistasio@gmail.com>
2018-12-10 22:35:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+let renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
 let renderSearchResult = (result) => {
     return `
         <div>
