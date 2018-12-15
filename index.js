7b8d7870adb4e81f177475fa9614d98804152e36
BrettSWeisberg <weisberg.brett@gmail.com>
2018-12-15 12:37:50 -0500

    Done.

--- index.js
+++ index.js
@@ -2,22 +2,47 @@ $(document).ready(function (){
 });
 
 function searchRepositories () {
-  function returnRepositories () {
-    const req = new XMLHttpRequest();
-    const searchTerms =  document.getElementById("searchTerms").value
-    req.addEventListener('load', showRepositories);
-    req.open('GET',`https:\/\/api.github.com\/search\/repositories\?q=${searchTerms}/`)
-    req.send();
-  };
-    function showRepositories() {
-      var repos = JSON.parse(this.responseText);
-      console.log(repos);
-const repoList = `<ul>${repos
-  .map(r => '<li>' + r.name + '</li>')
-  .join('')}</ul>`;
-document.getElementById('repositories').innerHTML = repoList;
-    }
-  debugger
-//  document.getElementById('results').innerHTML =
+    const searchTerm = document.getElementById("searchTerms").value
+    $.get(`https://api.github.com/search/repositories?q=${searchTerm}/`, function (data){
+      $("#result").html(showRepositories(data));
+    }).fail(error => displayError())
+  }
 
-}
+  function showRepositories(repos) {
+    console.log(repos);
+    const repoList = `<ul>${repos.items
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+    document.getElementById('results').innerHTML = repoList;
+  }
+
+  function getCommits(el) {
+    const owner = this.name;
+    const repo = el.dataset.repo;
+      $.get(`https://api.github.com/repos/${owner}/${repo}/commits/)`, function (data){
+        $("#details").html(showCommits(data));
+      }).fail(error => displayError())
+    }
+
+    function showCommits(commits) {
+      debugger
+      const commitsList = `<ul>${commits.map(commit =>
+         '<li><strong>' +
+         commit.commit.author.name + commit.url +
+        '</strong> - ' +
+         commit.commit.message +
+         '</li>'
+      ).join('')}</ul>`;
+    document.getElementById('details').innerHTML = commitsList;
+  }
+
+function displayError() {
+  document.getElementById('errors').innerHTML = 'error';
+  }
