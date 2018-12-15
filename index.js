71a040161afd13b8e0c7c94213aa24a8621e96c1
BrettSWeisberg <weisberg.brett@gmail.com>
2018-12-15 08:24:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 $(document).ready(function (){
 });
+
+function searchRepositories () {
+  function returnRepositories () {
+    const req = new XMLHttpRequest();
+    const searchTerms =  document.getElementById("searchTerms").value
+    req.addEventListener('load', showRepositories);
+    req.open('GET',`https:\/\/api.github.com\/search\/repositories\?q=${searchTerms}/`)
+    req.send();
+  };
+    function showRepositories() {
+      var repos = JSON.parse(this.responseText);
+      console.log(repos);
+const repoList = `<ul>${repos
+  .map(r => '<li>' + r.name + '</li>')
+  .join('')}</ul>`;
+document.getElementById('repositories').innerHTML = repoList;
+    }
+  debugger
+//  document.getElementById('results').innerHTML =
+
+}
