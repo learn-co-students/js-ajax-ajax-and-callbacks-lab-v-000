ee933d0f448dca6479c663f81ae5d9709cd3e6e0
btmccollum <btmcco8@gmail.com>
2019-01-02 14:46:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 $(document).ready(function (){
-  
 });
 
-function displayError() {
-    let message = "I'm sorry, there's been an error. Please try again."
-    document.getElementById('errors').innerHTML = message;
-}
+let displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.");
 
-function searchRepositories() {
-    const username = document.getElementById('searchTerms').value;
-    const query = `q=${username}`
-    $.get('https://api.github.com/search/repositories?' + query, function(response) {
+let searchRepositories = () => {
+    const username = $('#searchTerms').val();
+    $.get(`https://api.github.com/search/repositories?q=${username}`, function(response) {
         for (let i = 0; i < response.items.length; i++) {
             let result = response.items[i];
             let addition = `<p>
@@ -22,24 +17,24 @@ function searchRepositories() {
             </p>`;
             document.getElementById('results').innerHTML += addition;
         }
-    }); 
+    }).fail(error => {
+        displayError()
+    })
 }
 
-function showCommits(el) {
-    const owner = el.dataset.owner;
-    const repository = el.dataset.repository;
-    $.get(`https://api.github.com/repos/${owner}/${repository}/commits`, function(response) {
-        for (let i = 0; i < response.length; i++) {
-            console.log(response)
-            let result = response[i];
-            let addition = `
-            <p>
-                SHA: ${result.sha}<br>
-                <img src="${result.author.avatar_url}" style="height:40px;width:40px;">
-                Author: ${result.commit.author.name}<br>
-                Login: <strong>${result.author.login}:</strong> ${result.commit.message}
-            </p>`;
-            document.getElementById('details').innerHTML += addition;
-        }
-        });
+let renderCommit = (commit) => {
+    return `<li><img src="${commit.author.avatar_url}" style="height:40px;width:40px;"><h2>${commit.commit.author.name} (${commit.author.login})<h4>${commit.sha}</h4><p>${commit.commit.message}<p></li>`
+}
+
+let renderCommits = (data) => {
+    let result = data.map((commit) => renderCommit(commit)).join('')
+    return `<ul>${result}</ul>`
+}
+
+let showCommits = (el) => {
+    $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+        $('#details').html(renderCommits(data))
+    }).fail(error => {
+        displayError()
+    })
 }
