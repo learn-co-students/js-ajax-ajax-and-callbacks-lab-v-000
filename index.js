576cf3ab507ae0eca979d95e5a2b074f4f3959f3
Naomi Dennis <njdennis94@gmail.com>
2019-01-06 18:05:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 $(document).ready(function (){
+ 
 });
+
+function displayError(){
+  $("#error").html("An error has occured.")
+}
+
+function successful_server_call(){
+  console.log("SUCCESS")
+}
+
+function api_call(url, success_callback){
+  var jsXHR = $.get(url, successful_server_call)
+  jxXHR.fail( displayError ) 
+}
+function searchRepositories(){
+  let search_terms = $("#searchTerms").value() 
+  let api_root = "https://api.github.com/search/repositories?"
+  let query = `q=${search_terms}`
+  api_call(api_root + query, () =>{
+    $("#results").html(this.reponseText)
+  }.bind(this) )
+  
+}
+function showCommits(){
+  api_call(url)
+}
