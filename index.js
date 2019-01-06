3e1c5fa75acba22ee69053b6c7e9974762fe5b5e
Naomi Dennis <njdennis94@gmail.com>
2019-01-06 19:13:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 $(document).ready(function (){
  
 });
-
+function api_call(url, callbacks){
+  let jsxhr = $.get(url, () => {console.log("Success")})
+  jsxhr.fail( callbacks.fail )
+  jsxhr.done(callbacks.success)
+}
 function displayError(){
-  $("#error").html("An error has occured.")
+  $("#errors").html("error")
+
 }
 
-function successful_server_call(){
-  console.log("SUCCESS")
-}
-
-function api_call(url, success_callback){
-  var jsXHR = $.get(url, successful_server_call)
-  jxXHR.fail( displayError ) 
-}
 function searchRepositories(){
-  let search_terms = $("#searchTerms").value() 
-  let api_root = "https://api.github.com/search/repositories?"
-  let query = `q=${search_terms}`
-  api_call(api_root + query, () =>{
-    $("#results").html(this.reponseText)
-  }.bind(this) )
-  
+  let search_terms = $("#searchTerms").val()
+  let api_call_root = "https://api.github.com/search/repositories?"
+  let api_call_query = `q=${search_terms}`
+  let success = (resp) => {
+    search_terms = search_terms.split('') 
+    search_terms[0] = search_terms[0].toUpperCase()
+    search_terms = search_terms.join('')
+    $("#results").html(`${search_terms}<br/>Repos found: ${resp.total_count}`)
+    }
+  let fail = () => { displayError() }
+  api_call(api_call_root + api_call_query, {"success": success, "fail": fail})
 }
-function showCommits(){
-  api_call(url)
-}
+
+function showCommits(repo_details){
+//  let xhr = new XMLHttpRequest()
+//  xhr.setRequestHeader("Accept", "application/vnd.github.cloak-preview")
+  let api_call_root = `https://api.github.com/repos/${repo_details.dataset.owner}/${repo_details.dataset.repository}/commits/`
+  //let api_call_query = `q=repo:${repo_details.owner}/${repo_details.repository}`
+  let success = (resp) => {
+      let sha = resp[0].sha 
+      console.log(sha)
+      $("#details").html(sha)
+  }
+  let fail = () => { displayError() }
+  api_call(api_call_root, {"success": success, "fail": fail})
+  }
