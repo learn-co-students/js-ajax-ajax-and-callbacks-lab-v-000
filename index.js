4bb2631275a74100fcadd2fd4eae1debc0eded8e
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-13 21:31:50 +0000

    Done.

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ $(document).ready(function (){
 function searchRepositories(){
   const searchTerms = $('#searchTerms').val();
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
-    $('results').html(response);
-  })
+    const r = response.items;
+    let reposHTML = [];
+    r.forEach(function(item){
+      reposHTML += `
+      <div>
+        <h4>${item.name}</h4>
+        <p>${item.description}</p>
+        <a href="${item.html_url}">Go to repo</a>
+        <p>${item.owner.login}</p>
+        <img src="${item.owner.avatar_url}" width="25px">
+        <a href="${item.owner.url}">Go to owner</a>
+        <a href="#" data-owner="${item.owner.login}" data-repository="${item.name}" onclick="showCommits(this)">Show Commits</a>
+      </div>`;
+    })
+    $('#results').html(reposHTML);
+  }).fail(function(error){
+    displayError();
+  });
 }
 
 //display in details, listing the SHA, author, authors login, and avatar as an image
