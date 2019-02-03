b1f4b5cfabfee3eeb7e249350e12341055920495
epic0448 <samuelogundiran@Yahoo.com>
2019-02-02 23:22:06 -0500

    Done.

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ import { throws } from "assert";
 
 $(document).ready(function (){
 
-    $.get('index.html', function(response) {
-        $('#result').html(response);
-    });
+//     $.get('index.html', function(response) {
+//         $('#result').html(response);
+//     });
 
-    var url =
-    'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
+//     var url =
+//     'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
  
-   $.get(url).done(function(data) {
-    console.log('Done');
-    console.log(data);
-     });
+//    $.get(url).done(function(data) {
+//     console.log('Done');
+//     console.log(data);
+//      });
 
-    $.get('this_doesnt_exist.html', function(data) {
-        // This will not be called because the .html file request doesn't exist
-        doSomethingGood();
-            }).fail(function(error) {
-        // This is called when an error occurs
-        console.log('Something went wrong: ' + error.statusText);
-        });
+//     $.get('this_doesnt_exist.html', function(data) {
+//         // This will not be called because the .html file request doesn't exist
+//         doSomethingGood();
+//             }).fail(function(error) {
+//         // This is called when an error occurs
+//         console.log('Something went wrong: ' + error.statusText);
+//         });
 
 
 });
@@ -38,11 +38,11 @@ function searchRepositories() {
   }
 
   function showRepos(){
-       const repos = JSON.parse(this.responseText)
+        //const repos = JSON.parse(this.responseText)
     //    #iterate through this.repsonseText
     //    append to result div
-    const repoList = repos.map(r => `<li><a href=${r.html_url}>${r.name}</a></li>`).join('')
-    document.getElementsByTagName('result').innerHTML = "<ul>" + repoList + "</ul>";
+    //const repoList = repos.map(r => `<li><a href=${r.html_url}>${r.name}</a></li>`).join('')
+    //document.getElementsByTagName('result').innerHTML = "<ul>" + repoList + "</ul>";
   }
 
 
