7dff2c010d63bdbc6d6da3482fd5d3350c98f1e6
epic0448 <samuelogundiran@Yahoo.com>
2019-02-02 21:10:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 $(document).ready(function (){
+
+    // $.get('index.html', function(response) {
+    //     $('#result').html(response);
+    // });
+
+
 });
+
+function searchRepositories() {
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', showRepositories);
+    req.open('GET', 'https://api.github.com/users/octocat/repos');
+    req.send();
+  }
+
+
+  function showRepositories(event, data) {
+    const repos = JSON.parse(this.responseText);
+    const src = document.getElementById('results').innerHTML;
+    const template = Handlebars.compile(src);
+    const repoList = template(repos);
+    document.getElementById('repositories').innerHTML = repoList;
+  }
