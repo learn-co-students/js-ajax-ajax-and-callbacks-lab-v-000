087fc17f9b49444a313c4cefcdba580d67d15441
epic0448 <samuelogundiran@Yahoo.com>
2019-02-03 20:28:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-import { throws } from "assert";
+// import { create } from "domain";
 
-$(document).ready(function (){
-
-//     $.get('index.html', function(response) {
-//         $('#result').html(response);
-//     });
-
-//     var url =
-//     'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
- 
-//    $.get(url).done(function(data) {
-//     console.log('Done');
-//     console.log(data);
-//      });
-
-//     $.get('this_doesnt_exist.html', function(data) {
-//         // This will not be called because the .html file request doesn't exist
-//         doSomethingGood();
-//             }).fail(function(error) {
-//         // This is called when an error occurs
-//         console.log('Something went wrong: ' + error.statusText);
-//         });
-
+$(document).ready(function(){
 
 });
 
- function displayError(){
+ function displayError() {
      $('#errors').html("Im sorry, there's been an error.Please try again.");
  }
 function searchRepositories() {
@@ -38,17 +17,43 @@ function searchRepositories() {
   }
 
   function showRepos(){
-        //const repos = JSON.parse(this.responseText)
-    //    #iterate through this.repsonseText
-    //    append to result div
-    //const repoList = repos.map(r => `<li><a href=${r.html_url}>${r.name}</a></li>`).join('')
-    //document.getElementByTagId('#results').innerHTML = "<ul>" + repoList + "</ul>";
+        const repos = JSON.parse(this.responseText).items
+    const repoList = repos.map(r => `<li><a href=${r.html_url}>${r.name} </a> <a href="#" onclick="showCommits(this)" data-name=${r.name} data-owner=${r.owner.login}> show commits</p></p></li>`).join('')
+    
+    // const showRepos = repos.map(r => `<li><a href=${r.html_url}>${r.name}</a></li>`).join('')
+
+    document.getElementById('results').innerHTML = "<ul>" + repoList + "</ul>";
   }
 
 
-//   function showCommits(event, data) {
-//     const repos = JSON.parse(this.responseText);
-//     const src = document.getElementById('results').innerHTML;
+  function showCommits(element) {
+    document.getElementById('#repositories')
+    
+       $.get(`https://api.github.com/repos/${element.dataset.owner}/${element.name}/commits`,data => {
+        //    debugger
+           const commitsList = `<ul>${data.
+        map(
+            data => 
+             '<li><h3>' + data.commit.author.name + ' (' + data.author.login + ') </h3>' +
+            data.sha + "+  +"+'</li>' ).join('') }</ul>`
+        });
+    }
 
-//     document.getElementById('#repositories')
-//   }
+
+    //    debugger
+// <img src="img_chania.jpg" alt="Flowers in Chania">
+
+    //    display data from the , name, owner
+    // // Add a "Show Commits" link to each repository result that will call a showCommits function that gets the repository's commits from the GitHub API and display them in the details div. 
+   
+    // For each commit, list the SHA, the author, 
+    // the author's login, and the author's avatar as an image.
+ 
+// based on line 20
+    
+    
+// avatar_url: "https://avatars0.githubusercontent.com/u/96777?v=4"
+
+
+
+
