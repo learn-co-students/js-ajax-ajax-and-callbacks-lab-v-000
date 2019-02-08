ac5143977784b29a459d57b925674354912578bc
epic0448 <samuelogundiran@Yahoo.com>
2019-02-08 12:12:43 -0500

    Done.

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ $(document).ready(function(){
  }
 function searchRepositories(){
     let data = $("#searchTerms").val();
-
    let url = `https://api.github.com/search/repositories?q=${data}/`
+
     $.get(url,function(response){
-        var result = `<ul>${response.items.map(r => '<li>' + r.name + `</li><a href="#" data-owner="${r.owner.login}", data-result="${r.name}" onclick="showCommits(this)">Show Commits</a>`)}</ul>`
+        var result = `<ul>${response.items.map(r => '<li>' + r.name +
+                        `</li><a href="#" data-owner="${r.owner.login}",
+                            data-result="${r.name}" onclick="showCommits(this)">
+                               Show Commits</a>`)}</ul>`
        
         $("#results").html(result)
-     //$("#results").html(P.item);
 
     }    
    ) .fail(function(error){
@@ -33,31 +35,18 @@ function searchRepositories(){
     //   debugger
 
     document.getElementById('#repositories')
-
-       $.get(`https://api.github.com/repos/${element.dataset.owner}/${element.dataset.result}/commits`,data => {
-           debugger
-           const commitsList = `<ul>${data.
-        map(
-            data => 
-             '<li><h3>' + data.commit.author.name + ' (' + data.author + ') </h3>' +
-            data.sha + "+  +"+'</li>' ).join('') }</ul>`
+        let commitURL = `https://api.github.com/repos/${element.dataset.owner}/${element.dataset.result}/commits`
+       
+        $.get(commitURL,data => {
+            let commitsList = `<ul>${data.map( data = '<li><h3>' + data.commit.author.name + ' (' + data.author + ') </h3>' +
+             data.sha + "+  +"+'</li>' ).join('') }</ul>`
+            $("#details").html(commitsList) 
+          }
+           ).fail(function(error){
+             console.log(displayError)
         });
     }
 
-
-    //    debugger
-// <img src="img_chania.jpg" alt="Flowers in Chania">
-
-    //    display data from the , name, owner
-    // // Add a "Show Commits" link to each repository result that will call a showCommits function that gets the repository's commits from the GitHub API and display them in the details div. 
-   
-    // For each commit, list the SHA, the author, 
-    // the author's login, and the author's avatar as an image.
- 
-// based on line 20
-    
-    
-// avatar_url: "https://avatars0.githubusercontent.com/u/96777?v=4"
 
 
 
