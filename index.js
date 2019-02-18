bc3f00ef45bf1fc1c765ccfbe5fa9792f009d484
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-17 19:05:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const searchTerms = document.getElementById("searchTerms").value;
+  const searchURL = `https://api.github.com/search/repositories?q=${searchTerms}`;
+  $.get(searchURL, function(data) {
+    $("#results").append("Hello!");
+  });
+  // const xhr = new XMLHttpRequest();
+  // xhr.addEventListener('load', showRepositories);
+  // xhr.open('GET', searchURL);
+  // xhr.send();
+}
+
+function showRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList = repos;
+  document.getElementById("results").innerHTML = repoList;
+}
