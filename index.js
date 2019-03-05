fb8e2616613408a4d0146e20caa51e6cec44e7a5
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-03-05 16:33:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+    const searchTerms = $('#searchTerms').value
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+
+    let html = data.items.map(repo => {
+        `<div>
+            <h2>hi</h2>
+        </div>`
+    })
+    
+    $("#results").html(html)
+  })
+}
+
